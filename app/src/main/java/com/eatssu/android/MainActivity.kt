c6b585dbd0c82091a6ec0c9fb5458c0856ffbc88
Amepistheo <kellylhji@gmail.com>
2023-03-04 21:01:59 +0900

    add: Info 카카오 지도 API 연동 #2

--- app/src/main/java/com/eatssu/android/MainActivity.kt
+++ app/src/main/java/com/eatssu/android/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.eatssu.android
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64.NO_WRAP
+import android.util.Base64.encodeToString
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.eatssu.android.databinding.ActivityMainBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding : ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        Log.d("getKeyHash", "" + getKeyHash(this));
+    }
+    @SuppressLint("PackageManagerGetSignatures")
+    open fun getKeyHash(context: Context): String? {
+        val pm: PackageManager = context.getPackageManager()
+        try {
+            val packageInfo: PackageInfo =
+                pm.getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES)
+                    ?: return null
+            for (signature in packageInfo.signatures) {
+                try {
+                    val md = MessageDigest.getInstance("SHA")
+                    md.update(signature.toByteArray())
+                    return encodeToString(md.digest(), NO_WRAP)
+                } catch (e: NoSuchAlgorithmException) {
+                    e.printStackTrace()
+                }
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        return null
     }
 }
