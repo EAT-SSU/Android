fb6191eeae60d7c1f1adccf367f77cbb6d994274
Amepistheo <kellylhji@gmail.com>
2023-03-13 13:15:41 +0900

    add: 도담, 푸드코드, 스낵코너, 더키친, 기숙사식당 xml 구현 #2

---
+++ app/src/main/java/com/eatssu/android/InfoActivity_Kitchen.kt
@@ -0,0 +1,11 @@
+package com.eatssu.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoActivity_Kitchen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_kitchen)
+    }
+}
