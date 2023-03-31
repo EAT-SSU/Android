76be0e1e6fd23926cf70300699979243c8b47e6c
Yujin <jini1514@soongsil.ac.kr>
2023-04-01 01:38:32 +0900

    feat : retrofit 세팅 #6

---
+++ app/src/main/java/com/eatssu/android/data/RetrofitImpl.kt
@@ -0,0 +1,70 @@
+package com.eatssu.android.data
+
+
+import com.eatssu.android.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+
+object RetrofitImpl {
+    private const val BASE_URL = BuildConfig.BASE_URL
+
+    //토큰 없음
+    fun getApiClientWithOutToken(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(provideOkHttpClient(AppInterceptorWithOutToken()))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun provideOkHttpClient(interceptor: AppInterceptorWithOutToken): OkHttpClient
+            = OkHttpClient.Builder().run {
+        addInterceptor(interceptor)
+        build()
+    }
+
+    class AppInterceptorWithOutToken : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain) : Response = with(chain) {
+            val newRequest = request().newBuilder()
+                .addHeader("accept", "application/hal+json")
+                .addHeader("Content-Type", "application/json")
+                .build()
+            proceed(newRequest)
+        }
+    }
+
+
+    //토큰 있음
+    fun getApiClient(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(provideOkHttpClient(AppInterceptor()))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun provideOkHttpClient(interceptor: AppInterceptor): OkHttpClient
+            = OkHttpClient.Builder().run {
+        addInterceptor(interceptor)
+        build()
+    }
+
+    class AppInterceptor : Interceptor {
+
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain) : Response = with(chain) {
+
+            val newRequest = request().newBuilder()
+                .addHeader("accept", "application/hal+json")
+                .addHeader("Authorization","Bearer ${App.token_prefs.accessToken.toString()}")
+                .addHeader("Content-Type", "application/json")
+                .build()
+            proceed(newRequest)
+        }
+    }
+}
