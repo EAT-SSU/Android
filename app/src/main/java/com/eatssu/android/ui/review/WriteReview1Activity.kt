157eb0bd0f0b619e8f8f930d96ea898e801c9cf0
Yujin <jini1514@soongsil.ac.kr>
2023-05-25 00:43:03 +0900

    feat: tag done!!! #10 #19

--- app/src/main/java/com/eatssu/android/ui/review/WriteReview1Activity.kt
+++ app/src/main/java/com/eatssu/android/ui/review/WriteReview1Activity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.eatssu.android.App
 import com.eatssu.android.R
+import com.eatssu.android.data.enums.ReviewTag
 import com.eatssu.android.databinding.ActivityMyPageBinding
 import com.eatssu.android.databinding.ActivityWriteReview1Binding
 import com.eatssu.android.ui.BaseActivity
@@ -16,7 +17,7 @@ import com.eatssu.android.ui.BaseActivity
 class WriteReview1Activity : AppCompatActivity() {
     private lateinit var binding: ActivityWriteReview1Binding
 
-    private val selectedTags = mutableListOf<String>()
+    private val selectedTags = mutableListOf<ReviewTag>()
     private val maxTagSelection = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,35 +39,34 @@ class WriteReview1Activity : AppCompatActivity() {
 
 
         binding.btnChoose1.setOnClickListener(){
-//            handleTagSelection(ReviewTag.GOOD)
+            handleTagSelection(ReviewTag.GOOD)
         }
-
         binding.btnChoose2.setOnClickListener(){
-
+            handleTagSelection(ReviewTag.BAD)
         }
         binding.btnChoose3.setOnClickListener(){
-
+            handleTagSelection(ReviewTag.FULL)
         }
         binding.btnChoose4.setOnClickListener(){
-
+            handleTagSelection(ReviewTag.SOSO)
         }
         binding.btnChoose5.setOnClickListener(){
-
+            handleTagSelection(ReviewTag.SAD)
         }
         binding.btnChoose6.setOnClickListener(){
-
+            handleTagSelection(ReviewTag.SPICY)
         }
         binding.btnChoose7.setOnClickListener(){
-
+            handleTagSelection(ReviewTag.BLAND)
         }
         binding.btnChoose8.setOnClickListener(){
-
+            handleTagSelection(ReviewTag.SALTY)
         }
         binding.btnChoose9.setOnClickListener(){
-
+            handleTagSelection(ReviewTag.FAST)
         }
         binding.btnChoose10.setOnClickListener(){
-
+            handleTagSelection(ReviewTag.BIG)
         }
 
 
@@ -79,11 +79,12 @@ class WriteReview1Activity : AppCompatActivity() {
             intent.putExtra("menuId", MENU_ID)
             intent.putExtra("menu", menu)
 //            intent.putExtra("tag",selectedTags)
+            intent.putExtra("selectedTags", ArrayList(selectedTags))
             startActivity(intent)  // 화면 전환을 시켜줌
         }
     }
 
-    private fun handleTagSelection(tag: String) {
+    private fun handleTagSelection(tag: ReviewTag) {
         if (selectedTags.contains(tag)) {
             // Tag already selected, remove it
             selectedTags.remove(tag)
@@ -104,21 +105,47 @@ class WriteReview1Activity : AppCompatActivity() {
         }
     }
 
-    private fun updateTagButtonUI(tag: String, isSelected: Boolean) {
+    private fun updateTagButtonUI(tag: ReviewTag, isSelected: Boolean) {
         // Update the UI for the tag button based on selection state
         when (tag) {
-            "Tag1" -> binding.btnChoose1.isSelected = isSelected
-            // Repeat the above line for Tag2 to Tag10, replacing "Tag1" with the appropriate tag names.
+            ReviewTag.GOOD -> binding.btnChoose1.isSelected = isSelected
+            ReviewTag.BAD -> binding.btnChoose2.isSelected = isSelected
+            ReviewTag.FULL -> binding.btnChoose3.isSelected = isSelected
+            ReviewTag.SOSO -> binding.btnChoose4.isSelected = isSelected
+            ReviewTag.SAD -> binding.btnChoose5.isSelected = isSelected
+            ReviewTag.SPICY -> binding.btnChoose6.isSelected = isSelected
+            ReviewTag.BLAND -> binding.btnChoose7.isSelected = isSelected
+            ReviewTag.SALTY -> binding.btnChoose8.isSelected = isSelected
+            ReviewTag.FAST -> binding.btnChoose9.isSelected = isSelected
+            ReviewTag.BIG -> binding.btnChoose10.isSelected = isSelected
+
+
+            // Repeat the above line for the appropriate enum values, updating the corresponding button.
+            else -> {
+                false
+            }
         }
     }
 
+
+
     private fun initializeTagButtonUI() {
         // Initialize the UI for tag buttons based on initial selection state
-        // Replace "Tag1" to "Tag10" with the appropriate tag names.
-        binding.btnChoose1.isSelected = selectedTags.contains("Tag1")
-        // Repeat the above line for Tag2 to Tag10, replacing "Tag1" with the appropriate tag names.
-    }
+        // Replace ReviewTag.GOOD with the appropriate enum values, updating the corresponding button.
+        binding.btnChoose1.isSelected = selectedTags.contains(ReviewTag.GOOD)
+        binding.btnChoose2.isSelected = selectedTags.contains(ReviewTag.BAD)
+        binding.btnChoose3.isSelected = selectedTags.contains(ReviewTag.FULL)
+        binding.btnChoose4.isSelected = selectedTags.contains(ReviewTag.SOSO)
+        binding.btnChoose5.isSelected = selectedTags.contains(ReviewTag.SAD)
+        binding.btnChoose6.isSelected = selectedTags.contains(ReviewTag.SPICY)
+        binding.btnChoose7.isSelected = selectedTags.contains(ReviewTag.BLAND)
+        binding.btnChoose8.isSelected = selectedTags.contains(ReviewTag.SALTY)
+        binding.btnChoose9.isSelected = selectedTags.contains(ReviewTag.FAST)
+        binding.btnChoose10.isSelected = selectedTags.contains(ReviewTag.FULL)
 
+
+        // Repeat the above line for the appropriate enum values, updating the corresponding button.
+    }
 
 //
 //    override fun getLayoutResourceId(): Int {
