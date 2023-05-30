267d5f0e9e9b81b6417c16711aaefb53d6c5dae0
Yujin <jini1514@soongsil.ac.kr>
2023-05-30 22:53:29 +0900

    feat: 코드 정리

--- app/src/main/java/com/eatssu/android/ui/review/ReviewAdapter.kt
+++ app/src/main/java/com/eatssu/android/ui/review/ReviewAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.eatssu.android.data.model.response.GetMyReviewResponse
 import com.eatssu.android.data.model.response.GetReviewListResponse
 import com.eatssu.android.databinding.ItemReviewBinding
 import kotlin.math.min
