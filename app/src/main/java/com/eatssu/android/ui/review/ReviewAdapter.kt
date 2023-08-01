fccbdb9729a37fcbe6d3901e4da8dca906473174
Yujin <jini1514@soongsil.ac.kr>
2023-08-01 21:51:11 +0900

    feat: 이미지 바인딩 glide 사용

--- app/src/main/java/com/eatssu/android/ui/review/ReviewAdapter.kt
+++ app/src/main/java/com/eatssu/android/ui/review/ReviewAdapter.kt
@@ -1,11 +1,14 @@
 package com.eatssu.android.ui.review
 
+import  android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.eatssu.android.data.model.response.GetReviewListResponse
 import com.eatssu.android.databinding.ItemReviewBinding
-
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class ReviewAdapter(private val dataList: List<GetReviewListResponse.Data>?) :
     RecyclerView.Adapter<ReviewAdapter.ViewHolder>() {
@@ -16,12 +19,37 @@ class ReviewAdapter(private val dataList: List<GetReviewListResponse.Data>?) :
         fun bind(position: Int) {
             binding.tvReviewItemId.text = dataList?.get(position)?.writerNickname.toString()
             binding.tvReviewItemComment.text = dataList?.get(position)?.content
-            binding.tvReviewItemDate.text= dataList?.get(position)?.writeDate
-            binding.tvMenuName.text=dataList?.get(position)?.menu
+            binding.tvReviewItemDate.text = dataList?.get(position)?.writeDate
+            binding.tvMenuName.text = dataList?.get(position)?.menu
 
             binding.tvTotalRating.text = dataList?.get(position)?.mainGrade?.toFloat().toString()
             binding.tvTasteRating.text = dataList?.get(position)?.tasteGrade?.toFloat().toString()
             binding.tvAmountRating.text = dataList?.get(position)?.amountGrade?.toFloat().toString()
+
+
+// ... other code in your class ...
+
+// Assuming imgUrlList is a List<String>
+            val imgUrlList: List<String>? =
+                dataList?.get(position)?.imgUrlList// The list of image URLs
+
+// Get the ImageView reference from your layout
+            val imageView: ImageView =
+                binding.ivReviewPhoto// Replace R.id.imageView with the ID of your ImageView
+// Check if the imgUrlList is not empty before loading the image
+
+
+            if (dataList?.get(position)?.imgUrlList.isNullOrEmpty()) {
+                imageView.visibility = View.GONE
+            } else {
+                val imageUrl =
+                    imgUrlList?.get(0) // Assuming you want to load the first image from the list
+
+                Glide.with(itemView)
+                    .load(imageUrl)
+                    .into(imageView)
+                imageView.visibility = View.VISIBLE
+            }
         }
     }
 
