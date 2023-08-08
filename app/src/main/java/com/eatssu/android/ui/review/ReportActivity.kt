96c7328128e782432129f25296e75ef735750661
Amepistheo <kellylhji@gmail.com>
2023-08-08 23:57:17 +0900

    feat: 라디오버튼 선택시 서버에 넘어갈 번호 바꿔줌 #28

--- app/src/main/java/com/eatssu/android/ui/review/ReportActivity.kt
+++ app/src/main/java/com/eatssu/android/ui/review/ReportActivity.kt
@@ -1,10 +1,10 @@
 package com.eatssu.android.ui.review
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.eatssu.android.databinding.ActivityReportBinding
-import com.eatssu.android.databinding.ActivityReviewListBinding
 import com.eatssu.android.ui.BaseActivity
+import kotlin.properties.Delegates
 
 abstract class ReportActivity : BaseActivity() {
     private lateinit var binding: ActivityReportBinding
@@ -16,6 +16,23 @@ abstract class ReportActivity : BaseActivity() {
         setActionBarTitle("신고하기")
 
         setContentView(binding.root)
+
+        val radioGroup = binding.radioGp;
+        var radioNum = -1;
+        radioGroup.setOnClickListener {
+            if (binding.radioBt1.isChecked)
+                radioNum = 1;
+            else if (binding.radioBt2.isChecked)
+                radioNum = 2;
+            else if (binding.radioBt3.isChecked)
+                radioNum = 3;
+            else if (binding.radioBt4.isChecked)
+                radioNum = 4;
+            else if (binding.radioBt5.isChecked)
+                radioNum = 5;
+            else if (binding.radioBt6.isChecked)
+                radioNum = 6;
+        }
     }
 
     abstract fun setActionBarTitle(s: String)
