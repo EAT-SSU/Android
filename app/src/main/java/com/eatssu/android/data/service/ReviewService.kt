c5b4418e5341f4f8015e91bd17cd1c4fd3f70ea5
Yujin <jini1514@soongsil.ac.kr>
2023-08-15 15:08:51 +0900

    feat: 주석 코드 삭제

--- app/src/main/java/com/eatssu/android/data/service/ReviewService.kt
+++ app/src/main/java/com/eatssu/android/data/service/ReviewService.kt
@@ -37,41 +37,6 @@ interface ReviewService {
     ): Call<String>
 
 
-//    @GET("/review/info") //메뉴 리뷰 정보 조회(평점 등등)
-//    fun reviewInfo(@Query("menuType") menuType: String="FIX", @Query("menuId") menuId: Long, ): Call<GetReviewInfoResponseDto>
-//
-//    @GET("/review/info") //메뉴 리뷰 정보 조회(평점 등등)
-//    fun reviewInfo(@Query("menuType") menuType: String="CHANGE", @Query("mealId") mealId: Long): Call<GetReviewInfoResponseDto>
-
-
-    /*
-    @GET("/review/info") // Retrieve menu review information (rating, etc.) for fixed menus
-    fun reviewInfoForFixedMenu(
-        @Query("menuType") menuType: String = "FIX",
-        @Query("menuId") menuId: Long
-    ): Call<GetReviewInfoResponseDto>
-
-    @GET("/review/info") // Retrieve menu review information (rating, etc.) for changeable menus
-    fun reviewInfoForChangeableMenu(
-        @Query("menuType") menuType: String = "CHANGE",
-        @Query("mealId") mealId: Long
-    ): Call<GetReviewInfoResponseDto>
-
-    @GET("/review/list") //메뉴 리뷰 리스트 조회 - 고정메뉴
-    fun getReviewListForFixedMenu(
-        @Query("menuType") menuType: String = "FIX",
-        @Query("menuId") menuId: Long
-    ): Call<GetReviewListResponse>
-
-    @GET("review/list") //메뉴 리뷰 리스트 조회 - 가변메뉴
-    fun getReviewListForChangeableMenu(
-        @Query("menuType") menuType: String = "CHANGE",
-        @Query("mealId") mealId: Int,
-    ): Call<GetReviewListResponse>
-    */
-
-
-
     @GET("/review/info") // Retrieve menu review information (rating, etc.) for changeable menus
     fun getRreviewInfo(
         @Query("menuType") menuType: String,
