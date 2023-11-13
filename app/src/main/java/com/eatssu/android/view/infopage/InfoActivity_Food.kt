c2391edd15bc83bfd6cce477fbd634c0c095bc78
yujin <jini1514@soongsil.ac.kr>
2023-11-13 15:36:16 +0900

    fix: info 처리
    but. 하드 코딩

--- app/src/main/java/com/eatssu/android/view/infopage/InfoActivity_Food.kt
+++ app/src/main/java/com/eatssu/android/view/infopage/InfoActivity_Food.kt
@@ -19,8 +19,8 @@ import retrofit2.Retrofit
 
 class InfoActivity_Food : AppCompatActivity() {
     private lateinit var viewBinding: ActivityInfoFoodBinding
-    lateinit var retrofit: Retrofit
-    private lateinit var infoService: InfoService
+//    lateinit var retrofit: Retrofit
+//    private lateinit var infoService: InfoService
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,65 +28,65 @@ class InfoActivity_Food : AppCompatActivity() {
         viewBinding = ActivityInfoFoodBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        infoService = retrofit.create(InfoService::class.java)
+//        infoService = retrofit.create(InfoService::class.java)
 
-        val mapview = MapView(this);
-        val mapViewContainer = viewBinding.btnMap
-        mapViewContainer.addView(mapview)
-
-        // 중심점 변경 - 학생회관
-        mapview.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.497103,126.956785), true);
-        //mapview.zoomIn(true)
-
-        val marker = MapPOIItem()
-        marker.apply {
-            itemName = "학생회관"   // 마커 이름
-            mapPoint = MapPoint.mapPointWithGeoCoord(37.497103,126.956785)   // 좌표
-            markerType = MapPOIItem.MarkerType.RedPin
-            isCustomImageAutoscale = false      // 커스텀 마커 이미지 크기 자동 조정
-            setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
-        }
-        mapview.addPOIItem(marker)
-        getRestaurantInfo(Restaurant.FOOD_COURT)
+//        val mapview = MapView(this);
+//        val mapViewContainer = viewBinding.btnMap
+//        mapViewContainer.addView(mapview)
+//
+//        // 중심점 변경 - 학생회관
+//        mapview.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.497103,126.956785), true);
+//        //mapview.zoomIn(true)
+//
+//        val marker = MapPOIItem()
+//        marker.apply {
+//            itemName = "학생회관"   // 마커 이름
+//            mapPoint = MapPoint.mapPointWithGeoCoord(37.497103,126.956785)   // 좌표
+//            markerType = MapPOIItem.MarkerType.RedPin
+//            isCustomImageAutoscale = false      // 커스텀 마커 이미지 크기 자동 조정
+//            setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
+//        }
+//        mapview.addPOIItem(marker)
+//        getRestaurantInfo(Restaurant.FOOD_COURT)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun getRestaurantInfo(restaurantType: Restaurant) {
-
-        infoService.getRestaurantInfo(restaurantType.toString())
-            .enqueue(object : Callback<InfoResponseDto> {
-                override fun onResponse(
-                    call: Call<InfoResponseDto>,
-                    response: Response<InfoResponseDto>
-                ) {
-                    if (response.isSuccessful) {
-                        val body = response.body()
-                        body?.let {
-                            //putInfoResponse(it.openHours)
-                            viewBinding.tvFoodcourtLocation.text = it.location
-
-                            viewBinding.tvFoodcourtWeek.text = it.openHours[0].dayType
-                            viewBinding.tvLunchFoodcourt.text = it.openHours[0].timepart
-                            viewBinding.tvTimeFoodcourt.text = it.openHours[0].time
-                        }
-                        Log.d("response", "Response code : " + response.code())
-                    } else {
-                        Log.d("post", "onResponse 실패" + response.code())
-                    }
-                }
-
-                override fun onFailure(call: Call<InfoResponseDto>, t: Throwable) {
-                    Log.d("post", "onFailure 에러: ${t.message}")
-                }
-
-
-            })
-    }
-
-    private fun getNonFixed() {
-    }
-
-    override fun finish() {
-        super.finish()
-    }
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    private fun getRestaurantInfo(restaurantType: Restaurant) {
+//
+//        infoService.getRestaurantInfo(restaurantType.toString())
+//            .enqueue(object : Callback<InfoResponseDto> {
+//                override fun onResponse(
+//                    call: Call<InfoResponseDto>,
+//                    response: Response<InfoResponseDto>
+//                ) {
+//                    if (response.isSuccessful) {
+//                        val body = response.body()
+//                        body?.let {
+//                            //putInfoResponse(it.openHours)
+//                            viewBinding.tvFoodcourtLocation.text = it.location
+//
+//                            viewBinding.tvFoodcourtWeek.text = it.openHours[0].dayType
+//                            viewBinding.tvLunchFoodcourt.text = it.openHours[0].timepart
+//                            viewBinding.tvTimeFoodcourt.text = it.openHours[0].time
+//                        }
+//                        Log.d("response", "Response code : " + response.code())
+//                    } else {
+//                        Log.d("post", "onResponse 실패" + response.code())
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<InfoResponseDto>, t: Throwable) {
+//                    Log.d("post", "onFailure 에러: ${t.message}")
+//                }
+//
+//
+//            })
+//    }
+//
+//    private fun getNonFixed() {
+//    }
+//
+//    override fun finish() {
+//        super.finish()
+//    }
 }
