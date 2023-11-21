ee06f89fbbe27cf1ba8b4945c43bcb41996819d4
Amepistheo <kellylhji@gmail.com>
2023-11-21 20:03:18 +0900

    fix: 일단 리뷰 수정 오류 해결

--- app/src/main/java/com/eatssu/android/ui/review/etc/FixViewModel.kt
+++ app/src/main/java/com/eatssu/android/ui/review/etc/FixViewModel.kt
@@ -1,10 +1,14 @@
 package com.eatssu.android.ui.review.etc
 
 import android.annotation.SuppressLint
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.eatssu.android.data.service.ReviewService
 import com.eatssu.android.util.RetrofitImpl.retrofit
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Call
@@ -12,37 +16,27 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class FixViewModel : ViewModel() {
-    // LiveData to observe the result of the server communication
-    val resultLiveData = MutableLiveData<String>()
 
-    @SuppressLint("SuspiciousIndentation")
-    fun postData(reviewId: Long, comment: String, mainGrade: Int, amountGrade: Int, tasteGrade: Int) {
-        val service = retrofit.create(ReviewService::class.java)
+    private val _isDone = MutableLiveData<Boolean>()
+    val isDone: LiveData<Boolean> get() = _isDone
 
-        val reviewData = """
-            {
-                "mainGrade": $mainGrade,
-                "amountGrade": $amountGrade,
-                "tasteGrade": $tasteGrade,
-                "content": "$comment"
-            }
-        """.trimIndent().toRequestBody("application/json".toMediaTypeOrNull())
+    private val _toastMessage = MutableLiveData<String>()
+    val toastMessage: LiveData<String> get() = _toastMessage
 
-            service.modifyReview(reviewId, reviewData)
-                .enqueue(object : Callback<Void> {
-                    override fun onResponse(call: Call<Void>, response: Response<Void>) {
-                        if (response.isSuccessful) {
-                            if (response.code() == 200) {
-                                resultLiveData.value = "수정이 완료되었습니다."
-                            } else {
-                                resultLiveData.value = "수정이 실패하였습니다."
-                            }
-                        }
-                    }
+    fun handleSuccessResponse(message: String) {
+        viewModelScope.launch(Dispatchers.Main) {
+            _toastMessage.value = message
+            _isDone.value = true
 
-                    override fun onFailure(call: Call<Void>, t: Throwable) {
-                        resultLiveData.value = "수정이 실패하였습니다."
-                    }
-                })
         }
     }
+
+
+
+    fun handleErrorResponse(message: String) {
+        viewModelScope.launch(Dispatchers.Main) {
+            _toastMessage.value = message
+            _isDone.value = false
+        }
+    }
+}
--- app/src/main/java/com/eatssu/android/ui/review/etc/FixedReviewActivity.kt
+++ app/src/main/java/com/eatssu/android/ui/review/etc/FixedReviewActivity.kt
@@ -1,15 +1,14 @@
 package com.eatssu.android.ui.review.etc
-
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.eatssu.android.base.BaseActivity
-import com.eatssu.android.util.RetrofitImpl
 import com.eatssu.android.data.service.ReviewService
 import com.eatssu.android.databinding.ActivityFixMenuBinding
-import com.eatssu.android.ui.review.list.ReviewViewModel
+import com.eatssu.android.ui.review.etc.FixViewModel
+import com.eatssu.android.util.RetrofitImpl
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Call
@@ -18,8 +17,8 @@ import retrofit2.Response
 
 class FixedReviewActivity : BaseActivity<ActivityFixMenuBinding>(ActivityFixMenuBinding::inflate) {
 
+    private lateinit var viewModel : FixViewModel
     private var reviewId = -1L
-    private var menuId = -1L
     private lateinit var menu: String
     private var comment: String? = null
 
@@ -27,36 +26,63 @@ class FixedReviewActivity : BaseActivity<ActivityFixMenuBinding>(ActivityFixMenu
         super.onCreate(savedInstanceState)
         toolbarTitle.text = "리뷰 수정하기" // 툴바 제목 설정
 
-        menu = intent.getStringExtra("menu").toString()
-        binding.menu.text = menu
-        reportInfo()
-
-    }
-
-    private fun reportInfo() {
+        viewModel = ViewModelProvider(this).get(FixViewModel::class.java)
 
         reviewId = intent.getLongExtra("reviewId", -1L)
         Log.d("ReviewFixedActivity", reviewId.toString())
 
-        var viewModel = ViewModelProvider(this).get(FixViewModel::class.java)
-
-        val comment = binding.etReview2Comment.text.toString()
-        val mainGrade = binding.rbMain.rating.toInt()
-        val amountGrade = binding.rbAmount.rating.toInt()
-        val tasteGrade = binding.rbTaste.rating.toInt()
-
-        viewModel.resultLiveData.observe(this, Observer { result ->
-            Toast.makeText(this@FixedReviewActivity, result, Toast.LENGTH_SHORT).show()
-            if (result == "수정이 완료되었습니다.") {
-                finish()
-            }
-        })
+        menu = intent.getStringExtra("menu").toString()
+        binding.menu.text = menu
 
         binding.btnDone.setOnClickListener {
-
-            viewModel.postData(reviewId, comment, mainGrade, amountGrade, tasteGrade)
-            finish()
+            postData(reviewId)
         }
 
+        observeViewModel()
+    }
+
+    private fun postData(reviewId: Long) {
+        val service = RetrofitImpl.retrofit.create(ReviewService::class.java)
+
+        comment = binding.etReview2Comment.text.toString()
+
+        val reviewData = """
+        {
+            "mainGrade": ${binding.rbMain.rating.toInt()},
+            "amountGrade": ${binding.rbAmount.rating.toInt()},
+            "tasteGrade": ${binding.rbTaste.rating.toInt()},
+            "content": "$comment"
+        }
+    """.trimIndent().toRequestBody("application/json".toMediaTypeOrNull())
+
+        service.modifyReview(reviewId, reviewData)
+            .enqueue(object : Callback<Void> {
+                override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                    if (response.isSuccessful) {
+                        if (response.code() == 200) {
+                            viewModel.handleSuccessResponse("수정이 완료되었습니다.")
+
+                        } else {
+                            viewModel.handleErrorResponse("수정이 실패하였습니다.")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                    viewModel.handleErrorResponse("수정이 실패하였습니다.")
+                }
+            })
+    }
+
+    private fun observeViewModel() {
+        viewModel.toastMessage.observe(this, Observer { result ->
+            Toast.makeText(this@FixedReviewActivity, result, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.isDone.observe(this) { isDone ->
+            if(isDone) {
+                finish()
+            }
+        }
     }
 }
