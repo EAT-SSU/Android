87cbc1a0dc72b3750ee3c09712b7b0aa2324d51b
Amepistheo <kellylhji@gmail.com>
2024-01-19 13:59:07 +0900

    feat: 문의하기 xml 완료

---
+++ app/src/main/java/com/eatssu/android/ui/mypage/Inquire/InquireActivity.kt
@@ -0,0 +1,12 @@
+package com.eatssu.android.ui.mypage.Inquire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eatssu.android.R
+
+class InquireActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inquire)
+    }
+}
