1c78be81885eb6c6bce19c8ec2fe6f0fa72a46df
yujin <jini1514@soongsil.ac.kr>
2024-02-23 16:06:17 +0900

    delete: info service

--- app/src/main/java/com/eatssu/android/data/service/OauthService.kt
+++ app/src/main/java/com/eatssu/android/data/service/OauthService.kt
@@ -8,13 +8,16 @@ import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
 
-interface OauthService {
+interface OauthService { //여기는 토큰이 없는 레트로핏을 끼웁니다.
     @POST("oauths/reissue/token") //accessToken, refreshToken 재발급
     fun getNewToken(
-        @Header("Authorization") refreshToken: String?)
+        @Header("Authorization") refreshToken: String?,
+    ) //얘는 SP에 있는거 헤더에 넣어주면 됩니다.
             : Call<BaseResponse<TokenResponseDto>>
+
     @POST("oauths/kakao")
     fun loginWithKakao(
-        @Body request : LoginWithKakaoRequestDto)
+        @Body request: LoginWithKakaoRequestDto,
+    )
             : Call<BaseResponse<TokenResponseDto>>
 }
