36cddf8144865c19af226c140d9680b5c4ad9363
Amepistheo <kellylhji@gmail.com>
2024-03-01 17:59:00 +0900

    refactor : apply 함수로 변경 및 startActivity util로 변경

--- app/src/main/java/com/eatssu/android/ui/main/MainActivity.kt
+++ app/src/main/java/com/eatssu/android/ui/main/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.eatssu.android.R
-import com.eatssu.android.util.CalendarUtils
 import com.eatssu.android.base.BaseActivity
 import com.eatssu.android.data.service.UserService
 import com.eatssu.android.databinding.ActivityMainBinding
@@ -27,9 +26,11 @@ import com.eatssu.android.ui.mypage.MyPageActivity
 import com.eatssu.android.ui.mypage.MypageViewModel
 import com.eatssu.android.ui.mypage.MypageViewModelFactory
 import com.eatssu.android.ui.mypage.usernamechange.UserNameChangeActivity
+import com.eatssu.android.util.CalendarUtils
 import com.eatssu.android.util.CalendarUtils.daysInWeekArray
 import com.eatssu.android.util.CalendarUtils.monthYearFromDate
 import com.eatssu.android.util.RetrofitImpl
+import com.eatssu.android.util.extension.startActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.prolificinteractive.materialcalendarview.*
@@ -68,7 +69,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         viewPager.adapter = viewpagerFragmentAdapter
         viewPager.setCurrentItem(viewpagerFragmentAdapter.getDefaultFragmentPosition(), false)
 
-
         // ###### TabLayout과 ViewPager2를 연결
         // 1. 탭메뉴의 이름을 리스트로 생성해둔다.
         val tabTitles = listOf("아침", "점심", "저녁")
@@ -77,8 +77,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         TabLayoutMediator(tabLayout, viewPager) { tab, position -> tab.text = tabTitles[position] }.attach()
 
         binding.btnSetting.setOnClickListener {
-            val intent = Intent(this, MyPageActivity::class.java)  // 인텐트를 생성해줌,
-            startActivity(intent)  // 화면 전환을 시켜줌
+            startActivity<MyPageActivity>();
         }
 
         initWidgets()
@@ -123,30 +122,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         CalendarUtils.selectedDate = date
         calendarViewModel.setData(date)
         mainPosition = position
-        Log.d("maindate", date.toString())
 
-        // 1) ViewPager2 참조
         val viewPager: ViewPager2 = binding.vpMain
         val tabLayout: TabLayout = binding.tabLayout
 
-        // 2) FragmentStateAdapter 생성 : Fragment 여러개를 ViewPager2에 연결해주는 역할
         val viewpagerFragmentAdapter = ViewPager2Adapter(this)
 
-        // 3) ViewPager2의 adapter에 설정
         viewPager.adapter = viewpagerFragmentAdapter
         viewPager.setCurrentItem(viewpagerFragmentAdapter.getDefaultFragmentPosition(), false)
 
-
-        // ###### TabLayout과 ViewPager2를 연결
-        // 1. 탭메뉴의 이름을 리스트로 생성해둔다.
         val tabTitles = listOf("아침", "점심", "저녁")
 
-        // 2. TabLayout과 ViewPager2를 연결하고, TabItem의 메뉴명을 설정한다.
         TabLayoutMediator(tabLayout, viewPager) { tab, position -> tab.text = tabTitles[position] }.attach()
 
         binding.btnSetting.setOnClickListener {
-            val intent = Intent(this, MyPageActivity::class.java)  // 인텐트를 생성해줌,
-            startActivity(intent)  // 화면 전환을 시켜줌
+            startActivity<MyPageActivity>()
         }
 
         initWidgets()
--- app/src/main/java/com/eatssu/android/ui/main/calendar/CalendarAdapter.kt
+++ app/src/main/java/com/eatssu/android/ui/main/calendar/CalendarAdapter.kt
@@ -1,13 +1,10 @@
 package com.eatssu.android.ui.main.calendar
 
 import android.os.Build
-import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.eatssu.android.R
 import com.eatssu.android.databinding.ItemCalendarListBinding
@@ -15,7 +12,6 @@ import com.eatssu.android.util.CalendarUtils
 import java.time.LocalDate
 import java.time.format.TextStyle
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 internal class CalendarAdapter(
@@ -26,19 +22,24 @@ internal class CalendarAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
         val binding =
             ItemCalendarListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        val inflater = LayoutInflater.from(parent.context)
-        val view: View = inflater.inflate(R.layout.item_calendar_list, parent, false)
-        val layoutParams = view.layoutParams
-        layoutParams.height = parent.height
-        layoutParams.width = Gravity.RIGHT
-        return CalendarViewHolder(binding, view, onItemListener, days)
+
+        return LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_calendar_list, parent, false)
+            .apply {
+                layoutParams?.height = parent.height
+            }
+            .run {
+                CalendarViewHolder(binding, this, onItemListener, days)
+            }
     }
+
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
         val date = days[position]
         holder.dayOfMonth.text = date.dayOfMonth.toString()
-        holder.dayText.text = date.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN).toString()
+        holder.dayText.text =
+            date.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN).toString()
 
         if (date == CalendarUtils.selectedDate) {
             holder.parentView.setBackgroundResource(R.drawable.selector_background_blue)
