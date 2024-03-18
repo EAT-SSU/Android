d0f15c7ef96602fda046f4ef40bedad77717dee9
yujin <jini1514@soongsil.ac.kr>
2024-03-19 04:03:04 +0900

    add: 정말탈퇴하시겠어요? 파일추가

---
+++ app/src/main/java/com/eatssu/android/ui/mypage/withdraw/UserWithDrawActivity.kt
@@ -0,0 +1,13 @@
+package com.eatssu.android.ui.mypage.withdraw
+
+import android.os.Bundle
+import com.eatssu.android.base.BaseActivity
+import com.eatssu.android.databinding.ActivityUserWithDrawBinding
+
+class UserWithDrawActivity :
+    BaseActivity<ActivityUserWithDrawBinding>(ActivityUserWithDrawBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
