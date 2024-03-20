d7cbe8520e9cccc82317e400eee2ba0aa60c96c8
yujin <jini1514@soongsil.ac.kr>
2024-03-21 03:39:55 +0900

    fix: 내가 쓴 리뷰 인덱스 0 고정 취소

--- app/src/main/java/com/eatssu/android/ui/review/write/ImageViewModel.kt
+++ app/src/main/java/com/eatssu/android/ui/review/write/ImageViewModel.kt
@@ -4,12 +4,13 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.eatssu.android.App
 import com.eatssu.android.base.BaseResponse
 import com.eatssu.android.data.dto.response.ImageResponse
 import com.eatssu.android.data.service.ImageService
-import id.zelory.compressor.Compressor
-import id.zelory.compressor.constraint.quality
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
@@ -28,6 +29,10 @@ class ImageViewModel(
     private var _imageFile = MutableLiveData<File>()
     val imageFile: LiveData<File> get() = _imageFile
 
+    private val _uiState: MutableStateFlow<ImageState> = MutableStateFlow(ImageState())
+    val uiState: StateFlow<ImageState> = _uiState.asStateFlow()
+
+
     fun setImageFile(imageFile: File) {
         _imageFile.value = imageFile
     }
@@ -37,23 +42,19 @@ class ImageViewModel(
         _imageUrl.value = ""
     }
 
-    private suspend fun compressImage(imageFile: File): MultipartBody.Part {
 
-        val compressedFile = Compressor.compress(App.appContext, imageFile) { quality(50) }
-        val requestFile = compressedFile.asRequestBody("image/*".toMediaTypeOrNull())
+    fun saveS3() {
+//        val compressImageString = imageFile.value?.let { compressImage(it) }
 
-        return MultipartBody.Part.createFormData(
-            "image",
-            compressedFile.name,
-            requestFile
-        )
-    }
+        if (imageFile.value?.exists() == true) {
+            val requestFile = imageFile.value!!.asRequestBody("image/*".toMediaTypeOrNull())
+            val multipart = MultipartBody.Part.createFormData(
+                "image",
+                imageFile.value!!.name,
+                requestFile
+            )
 
-    suspend fun saveS3() {
-        val compressImageString = imageFile.value?.let { compressImage(it) }
-
-        if (compressImageString != null) {
-            imageService.getImageUrl(compressImageString).enqueue(
+            imageService.getImageUrl(multipart).enqueue(
                 object : Callback<BaseResponse<ImageResponse>> {
                     override fun onResponse(
                         call: Call<BaseResponse<ImageResponse>>,
@@ -62,12 +63,28 @@ class ImageViewModel(
                         if (response.isSuccessful) {
                             // 정상적으로 통신이 성공된 경우
                             _imageUrl.value = response.body()?.result?.url.toString()
-                            Log.d("ImageViewModel", _imageUrl.value.toString())
-
-
+                            Log.d("ImageViewModel", "이미지 변환 완료")
+                            _uiState.update {
+                                it.copy(
+                                    loading = false,
+                                    error = false,
+                                    isDone = true,
+                                    imgUrl = response.body()?.result?.url.toString()
+                                )
+                            }
                         } else {
                             // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
-                            Log.d("ImageViewModel", "onResponse 리뷰 작성 실패")
+                            _uiState.update {
+                                it.copy(
+                                    loading = false,
+                                    error = true,
+                                    isDone = false
+                                )
+                            }
+                            Log.d(
+                                "ImageViewModel",
+                                "이미지 변환 실패" + response.body()?.code + response.body()?.message
+                            )
                         }
                     }
 
@@ -80,6 +97,13 @@ class ImageViewModel(
                             "ImageViewModel",
                             "onFailure 에러: " + t.message.toString()
                         )
+                        _uiState.update {
+                            it.copy(
+                                loading = false,
+                                error = true,
+                                isDone = false
+                            )
+                        }
                     }
                 })
         }
@@ -87,3 +111,10 @@ class ImageViewModel(
 }
 
 
+data class ImageState(
+    var loading: Boolean = true,
+    var error: Boolean = false,
+
+    var imgUrl: String = "",
+    var isDone: Boolean = false,
+)
