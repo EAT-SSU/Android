b3187b869a9e860ae521791adfa77bd8b95d51c4
Yu Jin <qldls0307@naver.com>
2024-03-28 23:53:09 +0900

    feat: usecase

---
+++ app/src/main/java/com/eatssu/android/data/usecase/GetMealUseCase.kt
@@ -0,0 +1,14 @@
+package com.eatssu.android.data.usecase
+
+import com.eatssu.android.base.BaseResponse
+import com.eatssu.android.data.dto.response.MyReviewResponse
+import com.eatssu.android.data.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetMealUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+) {
+    suspend operator fun invoke(): Flow<BaseResponse<MyReviewResponse>> =
+        userRepository.getUserReviews()
+}
