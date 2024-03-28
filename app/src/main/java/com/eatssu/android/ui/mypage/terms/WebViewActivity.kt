70030954e65c5be97c1af6d4fd215a619f93b5c5
Yu Jin <qldls0307@naver.com>
2024-03-28 16:04:32 +0900

    chore: 수정 중

--- app/src/main/java/com/eatssu/android/ui/mypage/terms/WebViewActivity.kt
+++ app/src/main/java/com/eatssu/android/ui/mypage/terms/WebViewActivity.kt
@@ -1,6 +1,7 @@
 package com.eatssu.android.ui.mypage.terms
 
 import android.os.Bundle
+import android.webkit.WebViewClient
 import com.eatssu.android.base.BaseActivity
 import com.eatssu.android.databinding.ActivityWebviewBinding
 
@@ -15,8 +16,27 @@ class WebViewActivity : BaseActivity<ActivityWebviewBinding>(ActivityWebviewBind
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.webview.loadUrl(URL) // url 주소 가져오기}
-        binding.webview.settings.domStorageEnabled = true
+//        binding.webview.loadUrl(URL) // url 주소 가져오기}
+//        binding.webview.settings.domStorageEnabled = true
+        binding.webview.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
 
+            // localStorage 사용 시
+            // webView.settings.domStorageEnabled = true
+
+            if (savedInstanceState != null) restoreState(savedInstanceState)
+            else loadUrl(URL)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.webview.canGoBack()) binding.webview.goBack()
+        else super.onBackPressed()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.webview.saveState(outState)
     }
 }
