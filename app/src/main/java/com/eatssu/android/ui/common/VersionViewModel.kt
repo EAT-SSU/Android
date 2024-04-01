a5f0d6249e40894d608d65ae62dde229ef7306bb
Yu Jin <qldls0307@naver.com>
2024-04-02 01:44:27 +0900

    fix: 앱 업데이트 로직을 변경
    버전 네임 -> 버전 코드

--- app/src/main/java/com/eatssu/android/ui/common/VersionViewModel.kt
+++ app/src/main/java/com/eatssu/android/ui/common/VersionViewModel.kt
@@ -1,9 +1,10 @@
 package com.eatssu.android.ui.common
 
 import androidx.lifecycle.ViewModel
-import com.eatssu.android.BuildConfig.VERSION_NAME
+import com.eatssu.android.BuildConfig.VERSION_CODE
 import com.eatssu.android.data.model.AndroidMessage
 import com.eatssu.android.data.repository.FirebaseRemoteConfigRepository
+import timber.log.Timber
 
 class VersionViewModel(private val repository: FirebaseRemoteConfigRepository) : ViewModel() {
 
@@ -13,12 +14,28 @@ class VersionViewModel(private val repository: FirebaseRemoteConfigRepository) :
     }
 
     fun checkForceUpdate(): Boolean {
-        val lastVersion = checkAppVersion()
-        return VERSION_NAME != lastVersion && repository.getForceUpdate()
+
+        val versionCode = checkVersionCode() //얘가 파이어베이스에 있는 최신 버전
+        val thisCheckVersionCode = VERSION_CODE
+
+        Timber.d("앱의 versionCode는 " + thisCheckVersionCode + " 배포된 최신 버전은 " + versionCode)
+
+        if (thisCheckVersionCode < versionCode) { //배포된 버전이 크면 강제 업데이트
+            Timber.d("강제업데이트")
+            return true
+        } else if (thisCheckVersionCode >= versionCode) { //이 버전이 더 크거나 같으면 강제 업데이트 할 필요 x
+            Timber.d("업데이트 패스~")
+            return false
+        }
+        return false
     }
 
     fun checkAppVersion(): String {
         return repository.getAppVersion()
+    }
+
+    fun checkVersionCode(): Long {
+        return repository.getVersionCode()
     }
 
     fun checkAndroidMessage(): AndroidMessage {
