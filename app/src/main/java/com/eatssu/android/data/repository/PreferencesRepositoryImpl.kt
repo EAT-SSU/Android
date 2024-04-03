687fad68467b4752dbefc3e7c37b31ec03d4056c
Jin Yu <qldls0307@naver.com>
2024-04-04 03:14:53 +0900

    [Hotfix/token] 릴리즈 모드 / 디버그 모드 인터셉터 같게 함: 토큰 재발급이 안되던 이유 (#181)
    * feat: 디버그랑 릴리즈 인터셉터 같게함
    
    * version 1.1.12

---
+++ app/src/main/java/com/eatssu/android/data/repository/PreferencesRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.eatssu.android.data.repository
+
+//internal class PreferencesRepositoryImpl @Inject constructor(private val preferencesDataSource: SunPreferencesDataSource) :
+//    PreferencesRepository {
+//    override suspend fun getStoryCompleted(): Boolean = preferencesDataSource.getStoryCompleted()
+//
+//
+//    override suspend fun getAccessToken(): String? = preferencesDataSource.getAccessToken()
+//
+//    override suspend fun getRefreshToken(): String? = preferencesDataSource.getRefreshToken()
+//
+//    override suspend fun updateStoryCompleted(storyCompleted: Boolean) {
+//        preferencesDataSource.updateStoryCompleted(storyCompleted)
+//    }
+//
+//    override suspend fun updateAccessToken(accessToken: String?) {
+//        preferencesDataSource.updateAccessToken(accessToken)
+//    }
+//
+//    override suspend fun updateRefreshToken(refreshToken: String?) {
+//        preferencesDataSource.updateRefreshToken(refreshToken)
+//    }
+//
+//}
