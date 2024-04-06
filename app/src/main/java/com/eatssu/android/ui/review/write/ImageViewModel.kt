92737feaa5ecc3292ab6eb344384338621411507
Yu Jin <qldls0307@naver.com>
2024-04-06 21:34:12 +0900

    refactor: imageusecase not working

--- app/src/main/java/com/eatssu/android/ui/review/write/ImageViewModel.kt
+++ app/src/main/java/com/eatssu/android/ui/review/write/ImageViewModel.kt
@@ -1,34 +1,35 @@
 package com.eatssu.android.ui.review.write
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.eatssu.android.base.BaseResponse
-import com.eatssu.android.data.dto.response.ImageResponse
-import com.eatssu.android.data.service.ImageService
+import com.eatssu.android.data.usecase.GetImageUrlUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import timber.log.Timber
 import java.io.File
+import javax.inject.Inject
 
-//@HiltViewModel
+@HiltViewModel
 class ImageViewModel
-//@Inject constructor(
-//    private val getImageUrlUseCase: GetImageUrlUseCase,
-    (
-    private val imageService: ImageService,
+@Inject constructor(
+    private val getImageUrlUseCase: GetImageUrlUseCase,
+//    (
+//    private val imageService: ImageService,
 
-    ) : ViewModel() {
+) : ViewModel() {
 
     private val _imageUrl = MutableLiveData<String>()
     val imageUrl: LiveData<String> get() = _imageUrl
@@ -60,85 +61,86 @@ class ImageViewModel
                 requestFile
             )
             viewModelScope.launch {
-                imageService.getImageUrl(multipart).enqueue(
-                    object : Callback<BaseResponse<ImageResponse>> {
-                        override fun onResponse(
-                            call: Call<BaseResponse<ImageResponse>>,
-                            response: Response<BaseResponse<ImageResponse>>,
-                        ) {
-                            if (response.isSuccessful) {
-                                // 정상적으로 통신이 성공된 경우
-                                _imageUrl.value = response.body()?.result?.url.toString()
-                                Log.d("ImageViewModel", "이미지 변환 완료")
-                                _uiState.update {
-                                    it.copy(
-                                        loading = false,
-                                        error = false,
-                                        isDone = true,
-                                        imgUrl = response.body()?.result?.url.toString()
-                                    )
-                                }
-                            } else {
-                                // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
-                                _uiState.update {
-                                    it.copy(
-                                        loading = false,
-                                        error = true,
-                                        isDone = false
-                                    )
-                                }
-                                Log.d(
-                                    "ImageViewModel",
-                                    "이미지 변환 실패" + response.body()?.code + response.body()?.message
-                                )
-                            }
-                        }
-
-                        override fun onFailure(
-                            call: Call<BaseResponse<ImageResponse>>,
-                            t: Throwable,
-                        ) {
-                            // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
-                            Log.d(
-                                "ImageViewModel",
-                                "onFailure 에러: " + t.message.toString()
-                            )
-                            _uiState.update {
-                                it.copy(
-                                    loading = false,
-                                    error = true,
-                                    isDone = false
-                                )
-                            }
-                        }
-                    })
-            }
-
-//                getImageUrlUseCase(multipart).onStart {
-//                    _uiState.update { it.copy(loading = true) }
-//                }.onCompletion {
-//                    _uiState.update { it.copy(loading = false, error = true) }
-//                }.catch { e ->
-//                    _uiState.update { it.copy(error = true, toastMessage = "이미지 변환에 실패하였습니다.") }
-//                    Log.e(TAG, e.toString())
-//                }.collectLatest { result ->
-//                    Log.d(TAG, result.toString())
-//                    result.result?.apply {
-//                        _uiState.update {
-//                            it.copy(
-//                                loading = false,
-//                                error = false,
-//                                isDone = true,
-//                                imgUrl = url.toString()
-//                            )
+//                imageService.getImageUrl(multipart).enqueue(
+//                    object : Callback<BaseResponse<ImageResponse>> {
+//                        override fun onResponse(
+//                            call: Call<BaseResponse<ImageResponse>>,
+//                            response: Response<BaseResponse<ImageResponse>>,
+//                        ) {
+//                            if (response.isSuccessful) {
+//                                // 정상적으로 통신이 성공된 경우
+//                                _imageUrl.value = response.body()?.result?.url.toString()
+//                                Log.d("ImageViewModel", "이미지 변환 완료")
+//                                _uiState.update {
+//                                    it.copy(
+//                                        loading = false,
+//                                        error = false,
+//                                        isDone = true,
+//                                        imgUrl = response.body()?.result?.url.toString()
+//                                    )
+//                                }
+//                            } else {
+//                                // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+//                                _uiState.update {
+//                                    it.copy(
+//                                        loading = false,
+//                                        error = true,
+//                                        isDone = false
+//                                    )
+//                                }
+//                                Log.d(
+//                                    "ImageViewModel",
+//                                    "이미지 변환 실패" + response.body()?.code + response.body()?.message
+//                                )
+//                            }
 //                        }
 //
-//                    }
-//                }
+//                        override fun onFailure(
+//                            call: Call<BaseResponse<ImageResponse>>,
+//                            t: Throwable,
+//                        ) {
+//                            // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
+//                            Log.d(
+//                                "ImageViewModel",
+//                                "onFailure 에러: " + t.message.toString()
+//                            )
+//                            _uiState.update {
+//                                it.copy(
+//                                    loading = false,
+//                                    error = true,
+//                                    isDone = false
+//                                )
+//                            }
+//                        }
+//                    })
+//            }
 
-        }
+                getImageUrlUseCase(multipart).onStart {
+                    _uiState.update { it.copy(loading = true) }
+                }.onCompletion {
+                    _uiState.update { it.copy(loading = false, error = true) }
+                }.catch { e ->
+                    _uiState.update { it.copy(error = true, toastMessage = "이미지 변환에 실패하였습니다.") }
+                    Timber.e(e.toString())
+                }.collectLatest { result ->
+                    Timber.d(result.toString())
+                    result.result?.apply {
+                        _uiState.update {
+                            it.copy(
+                                loading = false,
+                                error = false,
+                                isDone = true,
+                                imgUrl = url.toString()
+                            )
+                        }
+
+                    }
+                }
+
+            }
         }
 
+    }
 }
 
 
