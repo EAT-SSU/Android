00e03376e3fed831861b3e6fcad9f8b77d0332ff
Yu Jin <qldls0307@naver.com>
2024-04-30 10:54:07 +0900

    chore: DialogUtil
    파일 다시 올려야함 날라감

--- app/src/main/java/com/eatssu/android/ui/review/delete/MyReviewDialogActivity.kt
+++ app/src/main/java/com/eatssu/android/ui/review/delete/MyReviewDialogActivity.kt
@@ -3,13 +3,11 @@ package com.eatssu.android.ui.review.delete
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.eatssu.android.databinding.ActivityMyReviewDialogBinding
 import com.eatssu.android.ui.review.modify.ModifyReviewActivity
+import com.eatssu.android.util.DialogUtil
 
 class MyReviewDialogActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMyReviewDialogBinding
@@ -55,30 +53,16 @@ class MyReviewDialogActivity : AppCompatActivity() {
         }
 
         binding.btnReviewDelete.setOnClickListener {
-            AlertDialog.Builder(this).apply {
-                setTitle("리뷰 삭제")
-                setMessage("작성한 리뷰를 삭제하시겠습니까?")
-                setNegativeButton("취소") { _, _ ->
-                    viewModel.handleErrorResponse("삭제를 취소하였습니다.")
-                }
-                setPositiveButton("삭제") { _, _ ->
-                    viewModel.postData(reviewId)
-                }
-            }.create().show()
-
-            observeViewModel()
+            showReviewDeleteDialog()
         }
     }
 
-    private fun observeViewModel() {
-        viewModel.toastMessage.observe(this, Observer { result ->
-            Toast.makeText(this@MyReviewDialogActivity, result, Toast.LENGTH_SHORT).show()
-        })
-
-        viewModel.isDone.observe(this) { isDone ->
-            if(isDone) {
-                finish()
-            }
+    private fun showReviewDeleteDialog() {
+        DialogUtil.createDialog(
+            "리뷰 삭제", "작성한 리뷰를 삭제하시겠습니까?", this@MyReviewDialogActivity,
+        ) { _, _ ->
+            viewModel.postData(reviewId)
+            finish()
         }
     }
 }
