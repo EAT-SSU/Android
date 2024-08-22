b42b0c5b452268a91d76f7bd6a0d0d77d1c83271
Yu Jin <qldls0307@naver.com>
2024-08-22 18:27:48 +0900

    fix: 사진 있을 때만 imageView&CardView 보이게

--- app/src/main/java/com/eatssu/android/ui/review/list/ReviewAdapter.kt
+++ app/src/main/java/com/eatssu/android/ui/review/list/ReviewAdapter.kt
@@ -31,26 +31,26 @@ class ReviewAdapter(private val dataList: List<Review>) :
                 binding.rbRate.rating = mainGrade.toFloat()
             }
 
-
-
             if (data.imgUrl?.size != 0) {
                 Log.d("ReviewAdapter", data.content + data.imgUrl?.size.toString())
                 Glide.with(itemView)
                     .load(data.imgUrl?.get(0))
                     .into(binding.ivReviewPhoto)
                 binding.ivReviewPhoto.visibility = View.VISIBLE
-                binding.ivReviewPhoto.clipToOutline = true
-
+                binding.ivReviewPhoto.visibility = View.VISIBLE
 
                 if (data.imgUrl?.get(0) == "") {
                     binding.ivReviewPhoto.visibility = View.GONE
+//                    binding.cvPhotoReview.visibility = View.GONE
 
                 }
                 if (data.imgUrl?.get(0) == null) {
                     binding.ivReviewPhoto.visibility = View.GONE
+//                    binding.cvPhotoReview.visibility = View.GONE
                 }
             } else {
                 binding.ivReviewPhoto.visibility = View.GONE
+//                binding.cvPhotoReview.visibility = View.GONE
             }
 
 
@@ -85,7 +85,6 @@ class ReviewAdapter(private val dataList: List<Review>) :
                 binding.tvReviewItemComment.text = content
                 binding.tvReviewItemDate.text = writeDate
 
-
                 binding.rbRate.rating = mainGrade.toFloat()
             }
 
@@ -95,16 +94,19 @@ class ReviewAdapter(private val dataList: List<Review>) :
                     .load(data.imgUrl?.get(0))
                     .into(binding.ivReviewPhoto)
                 binding.ivReviewPhoto.visibility = View.VISIBLE
+                binding.cvPhotoReview.visibility = View.VISIBLE
 
                 if (data.imgUrl?.get(0) == "") {
                     binding.ivReviewPhoto.visibility = View.GONE
-
+                    binding.cvPhotoReview.visibility = View.GONE
                 }
                 if (data.imgUrl?.get(0) == null) {
                     binding.ivReviewPhoto.visibility = View.GONE
+                    binding.cvPhotoReview.visibility = View.GONE
                 }
             } else {
                 binding.ivReviewPhoto.visibility = View.GONE
+                binding.cvPhotoReview.visibility = View.GONE
             }
 
 
