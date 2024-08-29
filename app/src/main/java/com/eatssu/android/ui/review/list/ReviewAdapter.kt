1860efaf381a6f6cadffb0d9a5b31068b8b592b3
Yu Jin <qldls0307@naver.com>
2024-08-29 22:50:14 +0900

    refactor: material menus로 신고/수정/삭제 분리

--- app/src/main/java/com/eatssu/android/ui/review/list/ReviewAdapter.kt
+++ app/src/main/java/com/eatssu/android/ui/review/list/ReviewAdapter.kt
@@ -1,22 +1,31 @@
 package com.eatssu.android.ui.review.list
 
+import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.MenuRes
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.eatssu.android.R
 import com.eatssu.android.data.model.Review
-import com.eatssu.android.databinding.ItemOthersReviewBinding
 import com.eatssu.android.databinding.ItemReviewBinding
-import com.eatssu.android.ui.review.delete.MyReviewDialogActivity
+import com.eatssu.android.ui.review.delete.DeleteViewModel
+import com.eatssu.android.ui.review.modify.ModifyReviewActivity
 import com.eatssu.android.ui.review.report.ReportActivity
 
 
 class ReviewAdapter(private val dataList: List<Review>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private lateinit var viewModel: DeleteViewModel
+
 
     inner class ViewHolder(private val binding: ItemReviewBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -26,7 +35,7 @@ class ReviewAdapter(private val dataList: List<Review>) :
                 binding.tvWriterNickname.text = writerNickname
                 binding.tvReviewItemComment.text = content
                 binding.tvReviewItemDate.text = writeDate
-                binding.tvMenuName.text = menu
+//                binding.tvMenuName.text = menu
 
                 binding.rbRate.rating = mainGrade.toFloat()
             }
@@ -53,11 +62,52 @@ class ReviewAdapter(private val dataList: List<Review>) :
 //                binding.cvPhotoReview.visibility = View.GONE
             }
 
-
-            binding.btnDetail.setOnClickListener {
+            binding.btnDetail.setOnClickListener { v: View ->
                 if (data.isWriter) {
+                    showMenu(binding.root.context, v, R.menu.menu_my_review, data)
+                } else {
+                    showMenu(binding.root.context, v, R.menu.menu_other_review, data)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        val binding =
+            ItemReviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolder -> holder.bind(position)
+        }
+    }
+
+
+    private fun showMenu(holdercontext: Context, v: View, @MenuRes menuRes: Int, data: Review) {
+        val popup = PopupMenu(holdercontext, v)
+        popup.menuInflater.inflate(menuRes, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            // Respond to menu item click.
+            when (menuItem.itemId) {
+                R.id.report -> {
                     val intent =
-                        Intent(binding.btnDetail.context, MyReviewDialogActivity::class.java)
+                        Intent(holdercontext, ReportActivity::class.java)
+                    intent.putExtra("reviewId", data.reviewId)
+                    intent.putExtra("menu", data.menu)
+                    ContextCompat.startActivity(holdercontext, intent, null)
+
+                    true
+                }
+
+                R.id.fix -> {
+                    val intent =
+                        Intent(holdercontext, ModifyReviewActivity::class.java)
                     intent.putExtra("reviewId", data.reviewId)
                     intent.putExtra("menu", data.menu)
                     intent.putExtra("content", data.content)
@@ -69,99 +119,37 @@ class ReviewAdapter(private val dataList: List<Review>) :
                     Log.d("ReviewFixedActivity", "전전:" + data.menu)
                     Log.d("ReviewFixedActivity", "전전:" + data.content)
 ////                    Timber.d("내용: "+data.content)
-                    ContextCompat.startActivity(binding.btnDetail.context, intent, null)
+                    ContextCompat.startActivity(holdercontext, intent, null)
+                    true
+                }
+
+                R.id.delete -> {
+                    AlertDialog.Builder(holdercontext).apply {
+                        setTitle("리뷰 삭제")
+                        setMessage("작성한 리뷰를 삭제하시겠습니까?")
+                        setNegativeButton("취소") { _, _ ->
+                            viewModel.handleErrorResponse("삭제를 취소하였습니다.")
+                        }
+                        setPositiveButton("삭제") { _, _ ->
+                            viewModel.postData(data.reviewId)
+                        }
+                    }.create().show()
+
+                    true
+                }
+
+                else -> {
+                    true
                 }
             }
         }
-    }
-
-    inner class OthersViewHolder(private val binding: ItemOthersReviewBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(position: Int) {
-
-            val data = dataList[position].apply {
-                binding.tvWriterNickname.text = writerNickname
-                binding.tvReviewItemComment.text = content
-                binding.tvReviewItemDate.text = writeDate
-
-                binding.rbRate.rating = mainGrade.toFloat()
-            }
-
-
-            if (data.imgUrl?.size != 0) {
-                Glide.with(itemView)
-                    .load(data.imgUrl?.get(0))
-                    .into(binding.ivReviewPhoto)
-                binding.ivReviewPhoto.visibility = View.VISIBLE
-                binding.cvPhotoReview.visibility = View.VISIBLE
-
-                if (data.imgUrl?.get(0) == "") {
-                    binding.ivReviewPhoto.visibility = View.GONE
-                    binding.cvPhotoReview.visibility = View.GONE
-                }
-                if (data.imgUrl?.get(0) == null) {
-                    binding.ivReviewPhoto.visibility = View.GONE
-                    binding.cvPhotoReview.visibility = View.GONE
-                }
-            } else {
-                binding.ivReviewPhoto.visibility = View.GONE
-                binding.cvPhotoReview.visibility = View.GONE
-            }
-
-
-            binding.tvReviewItemReport.setOnClickListener {
-                if (!data.isWriter) {
-                    val intent =
-                        Intent(binding.tvReviewItemReport.context, ReportActivity::class.java)
-                    intent.putExtra("reviewId", data.reviewId)
-                    intent.putExtra("menu", data.menu)
-                    ContextCompat.startActivity(binding.tvReviewItemReport.context, intent, null)
-                }
-            }
+        popup.setOnDismissListener {
+            // Respond to popup being dismissed.
         }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
-            VIEW_TYPE_MY_REVIEW -> {
-                val binding =
-                    ItemReviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-                ViewHolder(binding)
-            }
-
-            VIEW_TYPE_OTHERS_REVIEW -> {
-                val othersBinding = ItemOthersReviewBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
-                OthersViewHolder(othersBinding)
-            }
-
-            else -> throw IllegalArgumentException("Invalid view type")
-        }
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder) {
-            is ViewHolder -> holder.bind(position)
-            is OthersViewHolder -> holder.bind(position)
-        }
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return if (dataList[position].isWriter) {
-            VIEW_TYPE_MY_REVIEW
-        } else {
-            VIEW_TYPE_OTHERS_REVIEW
-        }
+//             Show the popup menu.
+        popup.show()
     }
 
     override fun getItemCount(): Int = dataList.size
 
-    companion object {
-        private const val VIEW_TYPE_MY_REVIEW = 1
-        private const val VIEW_TYPE_OTHERS_REVIEW = 2
-    }
 }
