e7cc7b6a5c50ff4ea4d568e0eabeb61de6bd4220
Yu Jin <qldls0307@naver.com>
2024-09-10 00:30:17 +0900

    fix: 아이콘 없대서 다시 되돌림

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.eatssu.android"
         minSdk = 23
         targetSdk = 34
-        versionCode = 17
-        versionName = "1.1.14"
+        versionCode = 19
+        versionName = "2.0.0"
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -44,7 +44,8 @@ android {
 
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-
+            var shrinkResources = false
+            var minifyEnabled = false
         }
 
         debug {
