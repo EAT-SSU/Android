086c84c3fb8521c6c019b33469f73ba6e394b589
Yu Jin <qldls0307@naver.com>
2024-10-10 11:33:19 +0900

    release/2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.eatssu.android"
         minSdk = 23
         targetSdk = 34
-        versionCode = 20
-        versionName = "2.1.0"
+        versionCode = 21
+        versionName = "2.1.1"
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
