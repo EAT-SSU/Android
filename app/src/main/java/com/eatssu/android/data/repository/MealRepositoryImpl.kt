65884569f3c7739bef69087f0d9b3eaeff81067e
Yu Jin <qldls0307@naver.com>
2025-02-25 23:27:14 +0900

    feat: worker 및 datastore

--- app/src/main/java/com/eatssu/android/data/repository/MealRepositoryImpl.kt
+++ app/src/main/java/com/eatssu/android/data/repository/MealRepositoryImpl.kt
@@ -70,6 +70,41 @@ class MealRepositoryImpl @Inject constructor(
         mealDataStore.saveMeal(date, restaurant, time, meal)
     }
 
+
+    override suspend fun fetchTodayMeal(
+        date: String,
+        restaurant: String,
+        time: String
+    ): Result<BaseResponse<ArrayList<GetMealResponse>>> {
+        return try {
+            // API 호출
+            val response = mealService.getTodayMeal2(date, restaurant, time)
+
+            if (response.isSuccess == true) {
+                val mealList = response.result ?: arrayListOf() // null이면 빈 리스트 반환
+                mealDataStore.saveMeal(mealList) // DataStore에 저장
+                Result.success(response) // BaseResponse 반환
+            } else {
+                Result.failure(Exception(response.message ?: "Unknown error"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+
+    override fun getTodayMealFlow(): Flow<ArrayList<GetMealResponse>> {
+        return mealDataStore.getMealFlow() // DataStore에서 Flow로 제공
+        //
+    }
+
+//    override suspend fun getTodayMeal2(
+//        date: String,
+//        restaurant: String,
+//        time: String
+//    ): Flow<BaseResponse<ArrayList<GetMealResponse>>> =
+//        flow { emit(mealService.getTodayMeal2(date, restaurant, time)) }
+
     override suspend fun getMenuInfoByMealId(mealId: Long): Flow<BaseResponse<MenuOfMealResponse>> =
         flow {
             emit(mealService.getMenuInfoByMealId(mealId))
