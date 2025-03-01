5204e3633925ec8d239e744ce7fbc4bea2006d72
Yu Jin <qldls0307@naver.com>
2025-03-01 20:17:40 +0900

    chore: enum -> 한글화 함수 추가

--- app/src/main/java/com/eatssu/android/data/enums/Time.kt
+++ app/src/main/java/com/eatssu/android/data/enums/Time.kt
@@ -1,5 +1,13 @@
 package com.eatssu.android.data.enums
 
-enum class Time {
-    MORNING, LUNCH, DINNER
+enum class Time(val displayName: String) {
+    MORNING("조식"),
+    LUNCH("중식"),
+    DINNER("석식");
+
+    companion object {
+        fun fromTimeEnumName(enumName: String): String {
+            return Time.values().find { it.name == enumName }?.displayName ?: ""
+        }
+    }
 }
