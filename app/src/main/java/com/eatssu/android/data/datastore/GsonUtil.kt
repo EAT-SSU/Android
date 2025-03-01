d3762b23d1aa72aaac29170a2254c6b1401c9750
Yu Jin <qldls0307@naver.com>
2025-03-01 17:29:01 +0900

    feat: Worker에서 Usecase로 API 호출하고, datastore에 저장

--- app/src/main/java/com/eatssu/android/data/datastore/GsonUtil.kt
+++ app/src/main/java/com/eatssu/android/data/datastore/GsonUtil.kt
@@ -1,21 +1,33 @@
 package com.eatssu.android.data.datastore
 
-import com.eatssu.android.data.dto.response.GetMealResponse
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import com.google.gson.reflect.TypeToken
 
-object GsonUtil {
 
-    fun <T> T.toJson(): String {
-        return Gson().toJson(this)
-    }
+fun <T> T.toJson(): String {
+    return Gson().toJson(this)
+}
 
-    inline fun <reified T> String.fromJson(): T {
-        return Gson().fromJson(this, T::class.java)
-    }
+inline fun <reified T> String.fromJson(): T {
+    return Gson().fromJson(this, T::class.java)
+}
 
-    fun String.fromJsonArray(): ArrayList<GetMealResponse> {
-        val type = object : TypeToken<ArrayList<GetMealResponse>>() {}.type
-        return Gson().fromJson(this, type)
+inline fun <reified T> String.fromJsonArray(): ArrayList<T> {
+    return try {
+        val gson = Gson()
+        val type = object : TypeToken<ArrayList<T>>() {}.type
+        gson.fromJson(this, type)
+    } catch (e: JsonSyntaxException) {
+        try {
+            // JSON이 배열이 아니라 단일 객체라면 리스트로 변환
+            val singleObject = Gson().fromJson<T>(this, T::class.java)
+            arrayListOf(singleObject)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            arrayListOf() // 변환 실패 시 빈 리스트 반환
+        }
     }
 }
+
+
