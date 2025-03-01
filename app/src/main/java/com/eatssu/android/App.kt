da1f4c43f24ad13530a4061cbc5c1b2da32c70b9
Yu Jin <qldls0307@naver.com>
2025-03-01 17:29:01 +0900

    feat: Worker에서 Usecase로 API 호출하고, datastore에 저장

--- app/src/main/java/com/eatssu/android/App.kt
+++ app/src/main/java/com/eatssu/android/App.kt
@@ -7,6 +7,8 @@ import androidx.work.Configuration
 import com.eatssu.android.domain.model.TokenState
 import com.eatssu.android.domain.model.TokenStateManager
 import com.eatssu.android.presentation.base.TokenEventBus
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.google.firebase.FirebaseApp
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
@@ -38,6 +40,10 @@ class App : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+
+//        // WorkManager 초기화
+//        WorkManager.initialize(this, Configuration.Builder().setWorkerFactory(workerFactory).build())
+
 
         appContext = this
         KakaoSdk.init(this,BuildConfig.KAKAO_NATIVE_APP_KEY)
--- app/src/main/java/com/eatssu/android/domain/usecase/meal/GetTodayMealUseCase.kt
+++ app/src/main/java/com/eatssu/android/domain/usecase/meal/GetTodayMealUseCase.kt
@@ -1,22 +1,30 @@
 package com.eatssu.android.domain.usecase.meal
 
-import com.eatssu.android.data.dto.response.BaseResponse
 import com.eatssu.android.data.dto.response.GetMealResponse
 import com.eatssu.android.domain.repository.MealRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class GetTodayMealUseCase @Inject constructor(
     private val mealRepository: MealRepository,
+    private val saveTodayMealUseCase: SaveTodayMealUseCase
 ) {
-    suspend fun execute(
+
+    suspend fun fetchMealData(
         date: String,
         restaurant: String,
         time: String
-    ): Result<BaseResponse<ArrayList<GetMealResponse>>> =
-        mealRepository.fetchTodayMeal(date, restaurant, time)
+    ):
+            Flow<ArrayList<GetMealResponse>> {
+        val data = mealRepository.fetchTodayMeal2(date, restaurant, time)
 
-    fun observeMeal(): Flow<ArrayList<GetMealResponse>> = mealRepository.getTodayMealFlow()
+// Flow를 List로 변환 후 저장
+        val mealList: List<GetMealResponse> = data.first()
+        saveTodayMealUseCase(mealList)
+        return data
+
+    }
 
 
 }
