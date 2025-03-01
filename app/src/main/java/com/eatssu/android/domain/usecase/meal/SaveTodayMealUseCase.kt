db3cbf923411952c8a1a3496902537b5a6bda3cf
Yu Jin <qldls0307@naver.com>
2025-03-01 18:26:38 +0900

    feat: data class WidgetMeal 만듦

--- app/src/main/java/com/eatssu/android/domain/usecase/meal/SaveTodayMealUseCase.kt
+++ app/src/main/java/com/eatssu/android/domain/usecase/meal/SaveTodayMealUseCase.kt
@@ -1,6 +1,5 @@
 package com.eatssu.android.domain.usecase.meal
 
-import com.eatssu.android.data.dto.response.GetMealResponse
 import com.eatssu.android.domain.repository.MealRepository
 import javax.inject.Inject
 
@@ -8,7 +7,12 @@ class SaveTodayMealUseCase @Inject constructor(
     private val mealRepository: MealRepository,
 ) {
 
-    suspend operator fun invoke(meal: List<GetMealResponse>) {
-        return mealRepository.saveTodayMeal(meal)
+    suspend operator fun invoke(
+        date: String,
+        restaurant: String,
+        time: String,
+        meal: List<String>
+    ) {
+        return mealRepository.saveTodayMeal(date, restaurant, time, meal)
     }
 }
