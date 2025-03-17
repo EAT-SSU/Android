5f48a6afc12bb0d35d10a11d7c29acfad8e034e9
yurikang <yuri3334@gmail.com>
2025-03-17 16:12:12 +0900

    [feat] compose color 세팅

---
+++ app/src/main/java/com/eatssu/android/presentation/compose/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.aurora.carevision.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Gray Scale
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Gray100 = Color(0xFFFAFAFB)
+val Gray200 = Color(0xFFF0F0F0)
+val Gray300 = Color(0xFFDEDEDE)
+val Gray400 = Color(0xFFA8A8A8)
+val Gray500 = Color(0xFF9D9D9D)
+val Gray600 = Color(0xFF565656)
+val Gray700 = Color(0xFF1F1F1F)
+
+// Brand
+val Primary = Color(0xFF66D4C2)
+val Secondary = Color(0xFFEEFBF8)
+val Star = Color(0xFFFFC700)
+val Error = Color(0xFFFF3F3F)
