b766b9e63f3a4ff52fd26753112a2959417dccf7
유진 <qldls0307@naver.com>
2025-03-27 22:21:57 +0900

    [Chore] Change delete/modify/report modal (#248)
    * add: bottomSheetDialogFragment
    
    * refactor: ListAdapter로 변경
    
    * change: bottomSheet clickListener
    
    * chore: 전체 리뷰에서 삭제하면 리스트 재호출 안되는거 수정
    
    * chore: 전체 리뷰에서 삭제하면 리스트 재호출 안되는거 수정
    
    * chore: 왼쪽 정렬, 워딩 수정 "리뷰 설정"
    
    * chore: 오탈자 수정
    
    * chore: apply로 바로 받아서 쓰기

--- app/src/main/java/com/eatssu/android/presentation/mypage/myreview/MyReviewAdapter.kt
+++ app/src/main/java/com/eatssu/android/presentation/mypage/myreview/MyReviewAdapter.kt
@@ -1,69 +1,61 @@
 package com.eatssu.android.presentation.mypage.myreview
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.bumptech.glide.Glide
 import com.eatssu.android.data.MySharedPreferences
 import com.eatssu.android.databinding.ItemReviewBinding
 import com.eatssu.android.domain.model.Review
-import com.eatssu.android.presentation.review.delete.MyReviewDialogActivity
 import timber.log.Timber
 
+class MyReviewAdapter :
+    ListAdapter<Review, MyReviewAdapter.ViewHolder>(ReviewDiffCallback()) {
 
-class MyReviewAdapter(private val dataList: List<Review>) :
-    RecyclerView.Adapter<MyReviewAdapter.ViewHolder>() {
+    interface OnItemClickListener {
+        fun onMyReviewClicked(view: View, reviewData: Review)
+    }
+
+    // 객체 저장 변수
+    private lateinit var mOnItemClickListener: OnItemClickListener
+
+    // 객체 전달 메서드
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
+        mOnItemClickListener = onItemClickListener
+    }
 
     inner class ViewHolder(private val binding: ItemReviewBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+        androidx.recyclerview.widget.RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(position: Int) {
-            binding.tvReviewItemComment.text = dataList[position].content
-            binding.tvReviewItemDate.text = dataList[position].writeDate
-            binding.tvMenuName.text = dataList[position].menu
-
-            binding.rbRate.rating = dataList[position].mainGrade.toFloat()
+        fun bind(data: Review) {
             binding.tvWriterNickname.text = MySharedPreferences.getUserName(binding.root.context)
+            binding.tvReviewItemComment.text = data.content
+            binding.tvReviewItemDate.text = data.writeDate
+            binding.tvMenuName.text = data.menu
+            binding.rbRate.rating = data.mainGrade.toFloat()
 
             val imageView: ImageView = binding.ivReviewPhoto
-
-            if (dataList[position].imgUrl?.isEmpty() == true) {
+            if (data.imgUrl?.isEmpty() == true || data.imgUrl?.get(0).isNullOrEmpty()) {
                 imageView.visibility = View.GONE
             } else {
-                Timber.d("사진 있다")
                 Glide.with(itemView)
-                    .load(dataList[position].imgUrl?.get(0))
+                    .load(data.imgUrl?.get(0))
                     .into(imageView)
                 imageView.visibility = View.VISIBLE
-
-                if (dataList[position].imgUrl?.get(0) == "" || dataList[position].imgUrl?.get(0) == null) {
-                    binding.ivReviewPhoto.visibility = View.GONE
-                }
             }
 
-            binding.btnDetail.setOnClickListener {
-                val intent = Intent(binding.btnDetail.context, MyReviewDialogActivity::class.java)
-                intent.putExtra("reviewId", dataList[position].reviewId)
-                intent.putExtra("menu", dataList[position].menu)
-
-                intent.putExtra("content", dataList[position].content)
-
-                intent.putExtra("mainGrade", dataList[position].mainGrade)
-                intent.putExtra("amountGrade", dataList[position].amountGrade)
-                intent.putExtra("tasteGrade", dataList[position].tasteGrade)
+            binding.btnDetail.setOnClickListener { v: View ->
+                mOnItemClickListener.onMyReviewClicked(v, data)
 
                 Timber.d(
                     "리뷰 상세 정보 - ID: %d, 메뉴: %s, 내용: %s",
-                    dataList[position].reviewId,
-                    dataList[position].menu,
-                    dataList[position].content
+                    data.reviewId,
+                    data.menu,
+                    data.content
                 )
-
-                ContextCompat.startActivity(binding.btnDetail.context, intent, null)
             }
         }
     }
@@ -75,8 +67,17 @@ class MyReviewAdapter(private val dataList: List<Review>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(position)
+        val item = getItem(position)
+        holder.bind(item)
+    }
+}
+
+class ReviewDiffCallback : DiffUtil.ItemCallback<Review>() {
+    override fun areItemsTheSame(oldItem: Review, newItem: Review): Boolean {
+        return oldItem.reviewId == newItem.reviewId
     }
 
-    override fun getItemCount(): Int = dataList.size
+    override fun areContentsTheSame(oldItem: Review, newItem: Review): Boolean {
+        return oldItem == newItem
+    }
 }
--- app/src/main/java/com/eatssu/android/presentation/mypage/myreview/MyReviewListActivity.kt
+++ app/src/main/java/com/eatssu/android/presentation/mypage/myreview/MyReviewListActivity.kt
@@ -3,18 +3,22 @@ package com.eatssu.android.presentation.mypage.myreview
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.eatssu.android.R
 import com.eatssu.android.databinding.ActivityMyReviewListBinding
 import com.eatssu.android.domain.model.Review
 import com.eatssu.android.presentation.base.BaseActivity
+import com.eatssu.android.presentation.common.MyReviewBottomSheetFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MyReviewListActivity :
-    BaseActivity<ActivityMyReviewListBinding>(ActivityMyReviewListBinding::inflate) {
+    BaseActivity<ActivityMyReviewListBinding>(ActivityMyReviewListBinding::inflate),
+    MyReviewBottomSheetFragment.OnReviewDeletedListener {
 
     private val myReviewViewModel: MyReviewViewModel by viewModels()
 
@@ -24,19 +28,30 @@ class MyReviewListActivity :
         super.onCreate(savedInstanceState)
         toolbarTitle.text = "내가 쓴 리뷰" // 툴바 제목 설정
 
-        lodeReview()
+        loadReview()
     }
 
     private fun setAdapter(reviewList: List<Review>) {
-        val listAdapter = MyReviewAdapter(reviewList)
+
+        val adapter = MyReviewAdapter()
+        adapter.submitList(reviewList)
+
         val linearLayoutManager = LinearLayoutManager(this)
 
-        binding.rvReview.adapter = listAdapter
+        adapter.setOnItemClickListener(object :
+            MyReviewAdapter.OnItemClickListener {
+
+            override fun onMyReviewClicked(view: View, reviewData: Review) {
+                onMyReviewClicked(review = reviewData)
+            }
+        })
+
+        binding.rvReview.adapter = adapter
         binding.rvReview.layoutManager = linearLayoutManager
         binding.rvReview.setHasFixedSize(true)
     }
 
-    private fun lodeReview() {
+    private fun loadReview() {
         myReviewViewModel.getMyReviews()
 
         lifecycleScope.launch {
@@ -55,11 +70,36 @@ class MyReviewListActivity :
 
     override fun onRestart() {
         super.onRestart()
-        lodeReview()
+        loadReview()
     }
 
     override fun onResume() {
         super.onResume()
-        lodeReview()
+        loadReview()
     }
+
+    fun onMyReviewClicked(review: Review) {
+
+        val modalBottomSheet = MyReviewBottomSheetFragment().apply {
+            arguments = Bundle().apply {
+                putLong("reviewId", review.reviewId)
+                putString("menu", review.menu)
+                putString("content", review.content)
+                putInt("mainGrade", review.mainGrade)
+                putInt("amountGrade", review.amountGrade)
+                putInt("tasteGrade", review.tasteGrade)
+            }
+            onReviewDeletedListener = this@MyReviewListActivity
+        }
+        modalBottomSheet.setStyle(
+            DialogFragment.STYLE_NORMAL,
+            R.style.RoundCornerBottomSheetDialogTheme
+        )
+        modalBottomSheet.show(supportFragmentManager, "Open Bottom Sheet")
+    }
+
+    override fun onReviewDeleted() {
+        loadReview()
+    }
+
 }
