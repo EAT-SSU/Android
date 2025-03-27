b7537f3a40e8f2e536bc8defd5b8d7f0ba25c940
yuri <83583757+kangyuri1114@users.noreply.github.com>
2025-03-27 15:04:12 +0900

    release 2.1.7 (#278)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.eatssu.android"
         minSdk = 23
         targetSdk = 34
-        versionCode = 26
-        versionName = "2.1.6"
+        versionCode = 27
+        versionName = "2.1.7"
 
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
