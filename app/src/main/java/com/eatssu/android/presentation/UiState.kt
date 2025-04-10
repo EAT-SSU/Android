0b3eba8252572a94fea6b2866d1103e460f05044
Yu Jin <qldls0307@naver.com>
2025-04-10 14:33:05 +0900

    chore: UiEvent, UiState (cherry pick)

---
+++ app/src/main/java/com/eatssu/android/presentation/UiState.kt
@@ -0,0 +1,15 @@
+package com.eatssu.android.presentation
+
+sealed interface UiState<out T> {
+    object Init : UiState<Nothing>
+
+    object Loading : UiState<Nothing>
+
+    data class Success<out T>(
+        val data: T? = null,
+    ) : UiState<T>
+
+    data class Error(
+        val errorMessage: String,
+    ) : UiState<Nothing>
+}
