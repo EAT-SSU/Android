7e4ec1729621e92fc972ad85a7d69d1be20534ce
kangyuri1114 <yuri3334@gmail.com>
2025-04-26 23:41:26 +0900

    feat: 불필요한 import 삭제

--- app/src/main/java/com/eatssu/android/domain/usecase/auth/LogoutUseCase.kt
+++ app/src/main/java/com/eatssu/android/domain/usecase/auth/LogoutUseCase.kt
@@ -1,13 +1,8 @@
 package com.eatssu.android.domain.usecase.auth
 
 import android.content.Context
-import android.content.Intent
-import android.widget.Toast
 import com.eatssu.android.data.MySharedPreferences
-import com.eatssu.android.presentation.login.LoginActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class LogoutUseCase @Inject constructor(
