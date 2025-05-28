d8b4a993e0f93f4d5ad13e54603012ee6f3f5f86
Yu Jin <qldls0307@naver.com>
2025-05-28 21:26:33 +0900

    delete: 미사용 파일

--- app/src/main/java/com/eatssu/android/data/dto/ApiResult.kt
+++
@@ -1,6 +0,0 @@
-package com.eatssu.android.data.dto
-
-sealed class ApiResult<T> {
-    data class Success<T>(val data: T?) : ApiResult<T>()
-    data class Failure(val message: String) : ApiResult<Nothing>()
-}
