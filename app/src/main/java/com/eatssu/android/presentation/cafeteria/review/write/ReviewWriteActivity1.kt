ddb85b5a4b25ca1bb78f9f733eb169be95fd8c85
Yu Jin <qldls0307@naver.com>
2025-05-29 00:05:26 +0900

    add: 리뷰 v2를 위한 compose 파일 세팅

---
+++ app/src/main/java/com/eatssu/android/presentation/cafeteria/review/write/ReviewWriteActivity1.kt
@@ -0,0 +1,47 @@
+package com.eatssu.android.presentation.cafeteria.review.write
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.eatssu.android.presentation.compose.ui.theme.EatssuTheme
+
+class ReviewWriteActivity1 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EatssuTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EatssuTheme {
+        Greeting("Android")
+    }
+}
