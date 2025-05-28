5b697e081452f23b72174b4a1c25cf544c19a373
Yu Jin <qldls0307@naver.com>
2025-05-28 17:11:18 +0900

    fix: 릴리즈 모드일 때만 Crashlytics, Analytics 수집 되도록

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,11 +47,13 @@ android {
             buildConfigField("String", "KAKAO_NATIVE_APP_KEY", "\"$kakaoKey\"")
             manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoKey
 
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-
             var shrinkResources = false
-            var minifyEnabled = false
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField("Boolean", "ENABLE_CRASHLYTICS", "true")
         }
 
         debug {
@@ -67,6 +69,9 @@ android {
             val kakaoKey: String = p.getProperty("KAKAO_NATIVE_APP_KEY")
             buildConfigField("String", "KAKAO_NATIVE_APP_KEY", "\"$kakaoKey\"")
             manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoKey
+
+            isMinifyEnabled = false
+            buildConfigField("Boolean", "ENABLE_CRASHLYTICS", "false")
         }
     }
 
