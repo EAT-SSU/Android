87e77cafc391a6c973cd0b681da95bf6f48939dc
yurikang <yuri3334@gmail.com>
2025-06-04 16:25:48 +0900

    feat: 만든 사람들 배경화면 색 도입

--- app/src/main/java/com/eatssu/android/presentation/mypage/DeveloperActivity.kt
+++ app/src/main/java/com/eatssu/android/presentation/mypage/DeveloperActivity.kt
@@ -1,10 +1,14 @@
 package com.eatssu.android.presentation.mypage
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import com.eatssu.android.R
 import com.eatssu.android.databinding.ActivityDeveloperBinding
 import com.eatssu.android.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.core.graphics.toColorInt
+import androidx.core.graphics.drawable.toDrawable
 
 @AndroidEntryPoint
 class DeveloperActivity :
@@ -13,5 +17,6 @@ class DeveloperActivity :
         super.onCreate(savedInstanceState)
 
         toolbarTitle.text = getString(R.string.developer) // 툴바 제목 설정
+        toolbar.background = "#B8E4FF".toColorInt().toDrawable()
     }
 }
