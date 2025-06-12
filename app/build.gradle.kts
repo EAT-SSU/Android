9f8f119f72c7918e239c5ca398329982ea2fd6e7
yurikang <yuri3334@gmail.com>
2025-06-12 09:57:29 +0900

    release: 2.1.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.eatssu.android"
         minSdk = 23
         targetSdk = 34
-        versionCode = 32
-        versionName = "2.1.12"
+        versionCode = 33
+        versionName = "2.1.13"
 
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
