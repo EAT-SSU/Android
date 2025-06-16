7200145946cbb7481f81f3864ff0bcec6c602dde
yurikang <yuri3334@gmail.com>
2025-06-16 17:15:18 +0900

    feat: 불필요 import문 삭제

--- app/src/main/java/com/eatssu/android/presentation/map/MapFragmentComposeView.kt
+++ app/src/main/java/com/eatssu/android/presentation/map/MapFragmentComposeView.kt
@@ -1,21 +1,15 @@
 package com.eatssu.android.presentation.map
 
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarDefaults.windowInsets
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.eatssu.android.presentation.compose.ui.theme.EatssuTheme
