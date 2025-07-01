79e0fc43e65df5dffe3c18dde8afe929f4347b6c
Yu Jin <qldls0307@naver.com>
2025-07-02 02:43:45 +0900

    feat: 위젯 클릭시 앱 실행

--- app/src/main/java/com/eatssu/android/presentation/widget/we/util/launchApp.kt
+++ app/src/main/java/com/eatssu/android/presentation/widget/we/util/launchApp.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import android.net.Uri
 
 fun Context.launchApp() {
-    val deepLinkUri = Uri.parse("onmi://root")
+    val deepLinkUri = Uri.parse("eatssu://root")
     val intent = Intent(Intent.ACTION_VIEW, deepLinkUri)
     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
