db74247a164378dc6cee7ab5751fd88662902744
Yu Jin <qldls0307@naver.com>
2025-07-02 02:12:59 +0900

    delete: save 관련 로직 제거

--- app/src/main/java/com/eatssu/android/domain/usecase/meal/GetTodayMealUseCase.kt
+++ app/src/main/java/com/eatssu/android/domain/usecase/meal/GetTodayMealUseCase.kt
@@ -35,28 +35,7 @@ sealed interface MealState {
 
 class GetTodayMealUseCase @Inject constructor(
     private val mealRepository: MealRepository,
-    private val saveTodayMealUseCase: SaveTodayMealUseCase
 ) {
-
-//    suspend operator fun invoke(
-//        date: String,
-//        restaurant: String,
-//        time: String
-//    ): Flow<ArrayList<GetMealResponse>> {
-//        val data = mealRepository.fetchTodayMeal(date, restaurant, time)
-//
-//        // Flow를 List로 변환 후 저장
-//        val mealList: List<GetMealResponse> = data.first()
-//
-//        // 각각의 메뉴를 합쳐서 식단 string으로 만듦
-//        val menuStrings = mealList.map { meal ->
-//            meal.briefMenus.joinToString("+") { it.name.toString() }
-//        }
-//
-//        saveTodayMealUseCase(date, restaurant, time, menuStrings)
-//        return data
-//
-//    }
 
     suspend operator fun invoke(
         date: String,
