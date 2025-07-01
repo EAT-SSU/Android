f0afeae9c53a256884808cce311ecae3f613375a
Yu Jin <qldls0307@naver.com>
2025-07-02 01:34:13 +0900

    feat: 시간 변경

--- app/src/main/java/com/eatssu/android/presentation/widget/medium/TodayMealReceiver.kt
+++
@@ -1,5 +0,0 @@
-package com.eatssu.android.presentation.widget.medium
-
-//class TodayMealReceiver : GlanceAppWidgetReceiver() {
-//    override val glanceAppWidget: GlanceAppWidget = TodayMealWidget()
-//}
--- app/src/main/java/com/eatssu/android/presentation/widget/small/TodayMealSmallReceiver.kt
+++
@@ -1,5 +0,0 @@
-package com.eatssu.android.presentation.widget.small
-
-//class TodayMealSmallReceiver : GlanceAppWidgetReceiver() {
-//    override val glanceAppWidget: GlanceAppWidget = TodayMealWidgetSmall()
-//}
