8a1b53240b79bf05fe5ecbb19a63bd7a7492a329
Yu Jin <qldls0307@naver.com>
2025-07-02 11:31:47 +0900

    feat: 색상 및 padding

--- app/src/main/java/com/eatssu/android/presentation/widget/we/theme/EATSSUWidgetColorScheme.kt
+++ app/src/main/java/com/eatssu/android/presentation/widget/we/theme/EATSSUWidgetColorScheme.kt
@@ -10,9 +10,9 @@ object EATSSUWidgetColorScheme {
     private val LightColorScheme = lightColorScheme(
         primary = Color(0xFF1F1F1F), // TextPrimary
         onPrimary = Color(0xFFFFFFFF), // BackgroundMain
-        secondary = Color(0xFF3E3D3F), // TextSecondary
-        onSecondary = Color(0xFFF8F8F8), // CardBackground,
-        tertiary = Color(0xFF000000) // Black
+        onBackground = Color(0xFFFAFAFB)
+//        onSecondary = Color(0xFFF8F8F8), // CardBackground,
+//        tertiary = Color(0xFF000000) // Black
     )
 
     private val DarkColorScheme = darkColorScheme(
@@ -25,6 +25,6 @@ object EATSSUWidgetColorScheme {
 
     val colors = ColorProviders(
         light = LightColorScheme,
-        dark = DarkColorScheme
+        dark = LightColorScheme
     )
 }
