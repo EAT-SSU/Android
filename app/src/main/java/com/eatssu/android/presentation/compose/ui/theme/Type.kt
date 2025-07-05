e9b58e8e0d177bebd43fc18f1b0022168828022e
kangyuri1114 <yuri696@naver.com>
2025-07-06 04:42:20 +0900

    feat: MapPin 컴포넌트 구현

--- app/src/main/java/com/eatssu/android/presentation/compose/ui/theme/Type.kt
+++ app/src/main/java/com/eatssu/android/presentation/compose/ui/theme/Type.kt
@@ -125,7 +125,7 @@ fun eatssuTypography(): EatssuTypography {
             lineHeight = 18.sp
         ),
         caption3 = EatssuTextStyle(
-            fontFamily = pretendardMedium,
+            fontFamily = pretendardRegular,
             fontSize = 10.sp,
             lineHeight = 12.sp
         ),
