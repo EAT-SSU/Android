f91739c29893875ec1a2671a4abaaafc7d9417b4
kangyuri1114 <yuri696@naver.com>
2025-07-06 15:23:07 +0900

    feat: 학과입력 BottomSheet 컴포즈 컴포넌트로 개발

---
+++ app/src/main/java/com/eatssu/android/presentation/map/component/MajorBottomSheet.kt
@@ -0,0 +1,135 @@
+package com.eatssu.android.presentation.map.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.eatssu.android.R
+import com.eatssu.android.presentation.compose.ui.theme.Black
+import com.eatssu.android.presentation.compose.ui.theme.EatssuTheme
+import com.eatssu.android.presentation.compose.ui.theme.Gray400
+import com.eatssu.android.presentation.compose.ui.theme.Primary
+import com.eatssu.android.presentation.compose.ui.theme.White
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MajorBottomSheet(
+    onDismiss: () -> Unit = {},
+    onInputClick: () -> Unit = {}
+) {
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+
+    LaunchedEffect(Unit) {
+        sheetState.show()
+    }
+
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        sheetState = sheetState,
+        dragHandle = null
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(White)
+        ) {
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .width(30.dp)
+                    .height(4.dp)
+                    .background(
+                        color = Gray400,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "학과를 입력하고\n나만의 제휴를 확인해보세요!",
+                style = EatssuTheme.typography.h2,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.padding(start = 24.dp)
+            )
+
+            Spacer(modifier = Modifier.height(81.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.img_new_logo_primary),
+                contentDescription = null,
+                modifier = Modifier.height(46.dp).align(Alignment.CenterHorizontally)
+            )
+
+            Row(
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ){
+                Text(
+                    text = "숭실대에서 ",
+                    style = EatssuTheme.typography.caption1,
+                    color = Black
+                )
+                Text(
+                    text = "먹자",
+                    style = EatssuTheme.typography.caption1,
+                    color = Primary
+                )
+            }
+
+            Spacer(modifier = Modifier.height(100.dp))
+
+            Button(
+                onClick = onInputClick,
+                colors = ButtonDefaults.buttonColors(containerColor = Primary),
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp, end = 24.dp, bottom = 6.dp)
+                    .height(52.dp)
+
+            ) {
+                Text("학과 입력하기", color = White, style = EatssuTheme.typography.button1)
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MajorBottomSheetPreview() {
+    EatssuTheme {
+        Surface {
+            MajorBottomSheet(
+                onDismiss = {},
+                onInputClick = {}
+            )
+        }
+    }
+}
+
