be6e65409d3a4521ff04e8c3f8171b2e64de1638
kangyuri1114 <yuri696@naver.com>
2025-07-07 02:09:21 +0900

    feat: 제휴지도, 학과조회 관련 Service, response 세팅

--- app/src/main/java/com/eatssu/android/data/dto/response/PartnershipResponse.kt
+++ app/src/main/java/com/eatssu/android/data/dto/response/PartnershipResponse.kt
@@ -1,21 +1,37 @@
 package com.eatssu.android.data.dto.response
 
+import com.google.gson.annotations.SerializedName
+
 data class PartnershipResponse(
+    @SerializedName("storeName")
     val storeName: String,
+    @SerializedName("longitude")
     val longitude: Double,
+    @SerializedName("latitude")
     val latitude: Double,
+    @SerializedName("restaurantType")
     val restaurantType: String,
+    @SerializedName("partnershipInfos")
     val partnershipInfos: List<PartnershipInfo>
 ){
     data class PartnershipInfo(
+        @SerializedName("id")
         val id: Int,
+        @SerializedName("partnershipType")
         val partnershipType: String,
+        @SerializedName("collegeName")
         val collegeName: String,
+        @SerializedName("departmentName")
         val departmentName: String,
+        @SerializedName("likeCount")
         val likeCount: Int,
+        @SerializedName("isLiked")
         val isLiked: Boolean,
+        @SerializedName("description")
         val description: String,
+        @SerializedName("startDate")
         val startDate: String,
+        @SerializedName("endDate")
         val endDate: String
     )
 }
--- app/src/main/java/com/eatssu/android/data/dto/response/PartnershipRestaurantResponse.kt
+++ app/src/main/java/com/eatssu/android/data/dto/response/PartnershipRestaurantResponse.kt
@@ -1,17 +1,32 @@
 package com.eatssu.android.data.dto.response
 
+import com.google.gson.annotations.SerializedName
+
 data class PartnershipRestaurantResponse(
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("partnershipType")
     val partnershipType: String,
+    @SerializedName("storeName")
     val storeName: String,
+    @SerializedName("description")
     val description: String,
+    @SerializedName("startDate")
     val startDate: String,
+    @SerializedName("endDate")
     val endDate: String,
+    @SerializedName("restaurantType")
     val restaurantType: String,
+    @SerializedName("longitude")
     val longitude: Double,
+    @SerializedName("latitude")
     val latitude: Double,
+    @SerializedName("collegeName")
     val collegeName: String,
+    @SerializedName("departmentName")
     val departmentName: String,
+    @SerializedName("partnershipLikeCount")
     val partnershipLikeCount: Int,
+    @SerializedName("likedByUser")
     val likedByUser: Boolean,
 )
