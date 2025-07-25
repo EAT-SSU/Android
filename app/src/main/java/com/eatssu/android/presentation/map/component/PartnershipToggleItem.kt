1869dd043fdafd7d7ead4dc7a1e518f4b1e7063b
kangyuri1114 <yuri696@naver.com>
2025-07-26 00:33:33 +0900

    feat: 토글 클릭 시 리플 효과 삭제

--- app/src/main/java/com/eatssu/android/presentation/map/component/PartnershipToggleItem.kt
+++ app/src/main/java/com/eatssu/android/presentation/map/component/PartnershipToggleItem.kt
@@ -3,6 +3,7 @@ package com.eatssu.android.presentation.map.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -73,7 +75,10 @@ fun PartnershipToggleItem(
             .clip(CircleShape)
             .background(backgroundColor)
             .padding(horizontal = 20.dp, vertical = 6.dp)
-            .clickable { onClick() },
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) { onClick() },
         contentAlignment = Alignment.Center
     ) {
         Text(
