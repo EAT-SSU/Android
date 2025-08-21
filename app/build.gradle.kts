e462111939045e2bc911b6af17bdf3a042c3b6eb
kangyuri1114 <yuri696@naver.com>
2025-08-22 00:59:45 +0900

    release: 2.1.15(35)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.eatssu.android"
         minSdk = 23
         targetSdk = 34
-        versionCode = 34
-        versionName = "2.1.14"
+        versionCode = 35
+        versionName = "2.1.15"
 
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
