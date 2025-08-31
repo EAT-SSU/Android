c656ceac81661c6cfd91d8d6b71e7ac4a38e4f63
Yu Jin <qldls0307@naver.com>
2025-08-31 13:51:34 +0900

    chore: change same minsdk

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 23
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
