6755b1691f7b48961c45d13b28b473f3d6e07430
Yu Jin <qldls0307@naver.com>
2025-08-31 16:50:23 +0900

    feat: 좋아요 버튼 분리

---
+++ app/src/main/java/com/eatssu/android/presentation/cafeteria/review/write/component/LikeButton.kt
@@ -0,0 +1,45 @@
+package com.eatssu.android.presentation.cafeteria.review.write.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.eatssu.android.presentation.compose.ui.theme.Gray300
+import com.eatssu.android.presentation.compose.ui.theme.Primary
+import com.eatssu.android.presentation.compose.ui.theme.White
+
+@Composable
+fun LikeButton(
+    isLiked: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val containerColor = if (isLiked) Primary else White
+    val contentColor = if (isLiked) White else Gray300
+    val borderColor = if (isLiked) Primary else Gray300
+
+    Button(
+        onClick = onClick,
+        modifier = modifier.size(58.dp, 28.dp),
+        shape = RoundedCornerShape(20.dp),
+        border = BorderStroke(1.dp, borderColor),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor
+        ),
+        contentPadding = PaddingValues(horizontal = 20.dp, vertical = 5.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = com.eatssu.android.R.drawable.ic_thumb_up), // R.drawable 경로가 올바른지 확인
+            contentDescription = "like",
+            modifier = Modifier.size(18.dp)
+        )
+    }
+}
