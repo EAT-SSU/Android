71080fb2f0752b37e66163692a66beba48cea399
Yu Jin <qldls0307@naver.com>
2025-09-05 00:10:42 +0900

    chore: 위젯 클릭시 바로가기

--- app/src/main/java/com/eatssu/android/presentation/widget/ui/MealWidget.kt
+++ app/src/main/java/com/eatssu/android/presentation/widget/ui/MealWidget.kt
@@ -257,11 +257,11 @@ class MealWidget : GlanceAppWidget() {
             modifier = GlanceModifier.fillMaxSize()
                 .background(GlanceTheme.colors.onPrimary)
                 .padding(16.dp)
-                .cornerRadius(20.dp),
-//                .clickable {
-//                    Timber.d("위젯 클릭")
-//                    context.launchApp()
-//                },
+                .cornerRadius(20.dp)
+                .clickable {
+                    Timber.d("위젯 클릭")
+                    context.launchApp()
+                },
         ) {
             Row(
                 modifier = GlanceModifier.fillMaxWidth(),
@@ -294,13 +294,6 @@ class MealWidget : GlanceAppWidget() {
             }
             Spacer(modifier = GlanceModifier.height(12.dp))
             content()
-            Box(
-                modifier = GlanceModifier
-                    .clickable {
-                        Timber.d("위젯 클릭")
-                        context.launchApp()
-                    },
-            ) {}
         }
     }
 
