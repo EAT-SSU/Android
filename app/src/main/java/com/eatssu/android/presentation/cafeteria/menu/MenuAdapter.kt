fefde9ebc1cc5f648d5b785b204e0c78fadc5f0f
Yu Jin <qldls0307@naver.com>
2025-09-06 23:19:44 +0900

    feat: logging click_menu

--- app/src/main/java/com/eatssu/android/presentation/cafeteria/menu/MenuAdapter.kt
+++ app/src/main/java/com/eatssu/android/presentation/cafeteria/menu/MenuAdapter.kt
@@ -46,7 +46,7 @@ class MenuAdapter(
                 setHasFixedSize(true)
                 layoutManager = LinearLayoutManager(binding.root.context)
                 adapter = sectionModel.menuList?.let {
-                    MenuSubAdapter(it, sectionModel.cafeteria.menuType)
+                    MenuSubAdapter(it, sectionModel.cafeteria)
                 }
             }
         }
