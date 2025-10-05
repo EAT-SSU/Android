faa18b390f0b954412b9ec11f36c4e6bebc47d42
정제훈 <pretocki3@gmail.com>
2025-10-05 16:25:38 +0900

    release: 3.1.2(40) (#390)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.eatssu.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 39
-        versionName = "3.1.1"
+        versionCode = 40
+        versionName = "3.1.2"
 
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
