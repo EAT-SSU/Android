f9373c6a40d519840b25b2ee2036c3c45c688a4d
PeraSite <pretocki3@gmail.com>
2025-10-17 17:33:40 +0900

    chore: ActivityUtil.kt startActivity deprecate

--- app/src/main/java/com/eatssu/android/presentation/util/ActivityUtil.kt
+++ app/src/main/java/com/eatssu/android/presentation/util/ActivityUtil.kt
@@ -4,6 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
+@Deprecated(
+    message = "Use Activity companion object pattern instead. Example: MainActivity.start(context)",
+    replaceWith = ReplaceWith("ActivityName.start(this)"),
+    level = DeprecationLevel.WARNING
+)
 inline fun <reified T : Activity> AppCompatActivity.startActivity(block: Intent.() -> Unit = {}) {
     startActivity(Intent(this, T::class.java).apply(block))
 }
