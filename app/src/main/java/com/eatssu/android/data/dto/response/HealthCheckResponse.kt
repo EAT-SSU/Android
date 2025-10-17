855d2293afba215a6e58b0a86d89c1efdbb4a218
PeraSite <pretocki3@gmail.com>
2025-10-17 13:16:57 +0900

    refactor: 미사용 Response 객체 삭제

--- app/src/main/java/com/eatssu/android/data/dto/response/HealthCheckResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.eatssu.android.data.dto.response
-
-import com.google.gson.annotations.SerializedName
-
-data class HealthCheckResponse(
-    @SerializedName("status")
-    val status: String
-)
-
