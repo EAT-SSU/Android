9ff0cb80869a2dd63f202db0b2e42c543a6b458d
PeraSite <pretocki3@gmail.com>
2025-10-21 19:32:56 +0900

    refactor: MainViewModel runCatching 리팩토링

--- app/src/main/java/com/eatssu/android/presentation/MainViewModel.kt
+++ app/src/main/java/com/eatssu/android/presentation/MainViewModel.kt
@@ -13,14 +13,12 @@ import com.eatssu.android.domain.usecase.user.GetUserNickNameUseCase
 import com.eatssu.android.domain.usecase.user.SetUserCollegeDepartmentUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.time.LocalDate
 import javax.inject.Inject
@@ -58,35 +56,26 @@ class MainViewModel @Inject constructor(
     private fun fetchAndCheckNickname() {
         viewModelScope.launch {
             _uiState.value = UiState.Loading
-            runCatching {
-                withContext(Dispatchers.IO) { getUserNickNameUseCase() }
-            }.onSuccess { nickname ->
-                // 1) 닉네임 없음/기본 프리셋
-                if (nickname.isNullOrBlank() || nickname.startsWith("user-")) {
-                    _uiState.value = UiState.Success(MainState.NicknameNull)
-                    _uiEvent.emit(UiEvent.ShowToast(context.getString(R.string.set_nickname)))
-                    return@launch // ← 아래 분기 실행 막기
-                }
 
-                // 2) 정상 닉네임
-                _uiState.value = UiState.Success(MainState.NicknameExists(nickname))
-                _uiEvent.emit(
-                    UiEvent.ShowToast(
-                        String.format(
-                            context.getString(R.string.hello_user),
-                            nickname
-                        )
-                    )
-                )
-            }.onFailure { e ->
-                _uiState.value = UiState.Error
-                _uiEvent.emit(
-                    UiEvent.ShowToast(
-                        context.getString(R.string.not_found)
-                    )
-                )
-                Timber.e(e)
+            val nickname = getUserNickNameUseCase()
+
+            // 1) 닉네임 없음/기본 프리셋
+            if (nickname.isBlank() || nickname.startsWith("user-")) {
+                _uiState.value = UiState.Success(MainState.NicknameNull)
+                _uiEvent.emit(UiEvent.ShowToast(context.getString(R.string.set_nickname)))
+                return@launch
             }
+
+            // 2) 정상 닉네임
+            _uiState.value = UiState.Success(MainState.NicknameExists(nickname))
+            _uiEvent.emit(
+                UiEvent.ShowToast(
+                    String.format(
+                        context.getString(R.string.hello_user),
+                        nickname
+                    )
+                )
+            )
         }
     }
 
