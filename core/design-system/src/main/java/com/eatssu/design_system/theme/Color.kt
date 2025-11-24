8b07a2d1452d9d59981db3dcb7563c6f1c76c2f4
Yu Jin <qldls0307@naver.com>
2025-11-24 22:23:36 +0900

    chore: color chart

--- core/design-system/src/main/java/com/eatssu/design_system/theme/Color.kt
+++ core/design-system/src/main/java/com/eatssu/design_system/theme/Color.kt
@@ -16,9 +16,29 @@ val Gray700 = Color(0xFF1F1F1F)
 // Brand
 val Primary = Color(0xFF66D4C2)
 val Secondary = Color(0xFFEEFBF8)
+
+// Yellow
 val Star = Color(0xFFFFC700)
+
+// Red
 val Error = Color(0xFFFF3F3F)
 
 // Special
 val KakaoLogin = Color(0xFFFEE500)
 
+// Alert
+val Info = Color(0xFF3D69FF)
+val InfoBg = Color(0xFFE7F4FE)
+val InfoBr = Color(0xFFD3EBFD)
+
+val Success = Color(0xFF228738)
+val SuccessBg = Color(0xFFEAF6EC)
+val SuccessBr = Color(0xFFD8EEDD)
+
+val Warning = Color(0xFFD17D00)
+val WarningBg = Color(0xFFFFF3DB)
+val WarningBr = Color(0xFFFFE0A3)
+
+val Danger = Color(0xFFDE3412)
+val DangerBg = Color(0xFFFDEFEC)
+val DangerBr = Color(0xFFFCDFD9)
