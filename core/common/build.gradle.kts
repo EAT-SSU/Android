21ac1472c4132f540c9bb0481c8203e7687160a3
Yu Jin <qldls0307@naver.com>
2025-11-25 16:28:30 +0900

    chore: material 의존성 제거

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.espresso.core)
