978995bfefbb128dc8c97bae3acc7aa6695e02b2
PeraSite <pretocki3@gmail.com>
2025-11-25 12:14:28 +0900

    refactor: unused import 삭제

--- app/src/main/java/com/eatssu/android/data/remote/repository/FirebaseRemoteConfigRepositoryImpl.kt
+++ app/src/main/java/com/eatssu/android/data/remote/repository/FirebaseRemoteConfigRepositoryImpl.kt
@@ -7,7 +7,6 @@ import com.eatssu.common.enums.Restaurant
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.tasks.await
 import timber.log.Timber
 import javax.inject.Inject
