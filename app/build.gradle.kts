6b51c4a1e5f5992468f23b9f56587ee6bfc68c6d
PeraSite <pretocki3@gmail.com>
2025-12-15 19:53:38 +0900

    feat: PostHog 설정 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,12 @@ android {
             buildConfigField("String", "NAVER_MAPS_CLIENT_ID", "\"$naverMapsClientID\"")
             manifestPlaceholders["NAVER_MAPS_CLIENT_ID"] = naverMapsClientID
 
+            val postHogApiKey: String = p.getProperty("POSTHOG_API_KEY")
+            buildConfigField("String", "POSTHOG_API_KEY", "\"$postHogApiKey\"")
+
+            val postHogHost: String = p.getProperty("POSTHOG_HOST")
+            buildConfigField("String", "POSTHOG_HOST", "\"$postHogHost\"")
+
             isShrinkResources = true
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
@@ -81,6 +87,12 @@ android {
             val naverMapsClientID: String = p.getProperty("NAVER_MAPS_CLIENT_ID")
             buildConfigField("String", "NAVER_MAPS_CLIENT_ID", "\"$naverMapsClientID\"")
             manifestPlaceholders["NAVER_MAPS_CLIENT_ID"] = naverMapsClientID
+
+            val postHogApiKey: String = p.getProperty("POSTHOG_API_KEY")
+            buildConfigField("String", "POSTHOG_API_KEY", "\"$postHogApiKey\"")
+
+            val postHogHost: String = p.getProperty("POSTHOG_HOST")
+            buildConfigField("String", "POSTHOG_HOST", "\"$postHogHost\"")
 
             isMinifyEnabled = false
         }
