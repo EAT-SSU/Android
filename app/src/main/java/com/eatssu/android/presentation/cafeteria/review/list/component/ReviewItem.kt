2bafaf9fcc5704ae80ed0f9bb1f660af90e9976d
Yu Jin <qldls0307@naver.com>
2025-12-21 15:38:33 +0900

    chore: 리뷰관리 점3개버튼과 작성일 끝점 맞춤

--- app/src/main/java/com/eatssu/android/presentation/cafeteria/review/list/component/ReviewItem.kt
+++ app/src/main/java/com/eatssu/android/presentation/cafeteria/review/list/component/ReviewItem.kt
@@ -1,20 +1,24 @@
 package com.eatssu.android.presentation.cafeteria.review.list.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -74,24 +78,33 @@ fun ReviewItem(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.End
             ) {
-                IconButton(
-                    onClick = { onMoreClick() }
+                Box(
+                    modifier = Modifier
+                        .padding(18.dp) // 터치 영역 확장 ( (48 - 12) / 2 )
+                        .offset(x = 18.dp, y = 18.dp) // ← 시각 위치 되돌리기
+                        .clickable(
+                            onClick = onMoreClick,
+                            indication = null,
+                            interactionSource = remember { MutableInteractionSource() }
+                        )
                 ) {
                     Icon(
                         painter = painterResource(id = R.drawable.ic_menu_12),
                         contentDescription = "etc",
                         modifier = Modifier.size(12.dp),
-                        tint = Color.Unspecified,
+                        tint = Color.Unspecified
                     )
                 }
 
+                Spacer(modifier = Modifier.height(8.dp))
+
                 Text(
-                    writeDate,
+                    text = writeDate,
                     style = EatssuTheme.typography.caption3,
-                    color = Gray400,
-                    modifier = Modifier.padding(end = 20.dp) //20인 이유는 없음 IconButton에 넣으면서 padding 생겨서 끝점을 맞추려고 조절한 것임
+                    color = Gray400
                 )
             }
+
         }
 
         Spacer(modifier = Modifier.height(8.dp))
