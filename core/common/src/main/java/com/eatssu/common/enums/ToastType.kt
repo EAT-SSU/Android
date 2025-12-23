3a1a60acf259036b053c5a0c6ba92445d4f038f1
PeraSite <pretocki3@gmail.com>
2025-12-23 09:52:00 +0900

    refactor: core 모듈로 Toast 관련 리소스 이동

--- core/common/src/main/java/com/eatssu/common/enums/ToastType.kt
+++ core/common/src/main/java/com/eatssu/common/enums/ToastType.kt
@@ -1,12 +1,17 @@
 package com.eatssu.common.enums
 
+import androidx.annotation.DrawableRes
+import com.eatssu.common.R
+
 /**
  * Toast 타입을 정의하는 Enum 클래스
- * common 모듈은 Android 리소스에 접근할 수 없으므로 순수한 타입 정보만 정의
  */
-enum class ToastType {
-    INFO,
-    SUCCESS,
-    WARNING,
-    ERROR
+enum class ToastType(
+    @field:DrawableRes val iconId: Int,
+    @field:DrawableRes val shapeId: Int
+) {
+    INFO(R.drawable.ic_toast_info, R.drawable.shape_toast_info),
+    SUCCESS(R.drawable.ic_toast_success, R.drawable.shape_toast_success),
+    WARNING(R.drawable.ic_toast_warning, R.drawable.shape_toast_warning),
+    ERROR(R.drawable.ic_toast_error, R.drawable.shape_toast_error)
 }
