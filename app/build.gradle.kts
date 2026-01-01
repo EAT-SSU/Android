15a4b906caaa97bf6e70c93d4ea2e99abed5863a
kangyuri1114 <yuri696@naver.com>
2026-01-02 02:58:27 +0900

    refactor: kapt -> ksp 마이그레이션, compose compiler 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,14 +3,14 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.google.services)
     alias(libs.plugins.firebase.crashlytics)
     alias(libs.plugins.hilt.android)
+    alias(libs.plugins.ksp)
     id("kotlin-parcelize")
     id("kotlin-android")
     id("com.google.android.gms.oss-licenses-plugin")
-    id("kotlin-kapt")
-
 }
 
 android {
@@ -103,10 +103,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
-    }
-
     kotlin {
         jvmToolchain(17)
     }
@@ -188,7 +184,7 @@ dependencies {
 
     //glide: 사진 업로드
     implementation(libs.glide)
-    kapt(libs.glide.compiler)
+    ksp(libs.glide.compiler)
 
     //coil: 이미지 로딩
     implementation(libs.coil.compose)
@@ -206,8 +202,8 @@ dependencies {
 
     // Hilt for Dependency Injection
     implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
-    kapt(libs.androidx.hilt.compiler)
+    ksp(libs.hilt.android.compiler)
+    ksp(libs.androidx.hilt.compiler)
     implementation(libs.androidx.hilt.common)
     implementation(libs.androidx.hilt.work)
     implementation(libs.hilt.navigation.compose)
@@ -253,8 +249,4 @@ dependencies {
     // PostHog
     implementation(libs.posthog.android)
 
-}
-
-kapt {
-    correctErrorTypes = true
 }
