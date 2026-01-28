cecae93e37d8b6e52941d82a7ce421066854a2af
Yu Jin <qldls0307@naver.com>
2026-01-28 15:21:04 +0900

    release: versionCode 49

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.eatssu.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 48
+        versionCode = 49
         versionName = "3.2.1"
 
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
