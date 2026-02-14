ed3ef1fc6faf535451a701d722edc4cddeb401e9
PeraSite <pretocki3@gmail.com>
2026-02-15 00:51:11 +0900

    test: meal menu partnership repository BDD 테스트를 추가했어요

---
+++ app/src/test/java/com/eatssu/android/data/remote/repository/MenuRepositoryImplBehaviorSpec.kt
@@ -0,0 +1,62 @@
+package com.eatssu.android.data.remote.repository
+
+import com.eatssu.android.data.model.ApiResult
+import com.eatssu.android.data.remote.dto.response.CategoryMenuListCollection
+import com.eatssu.android.data.remote.dto.response.GetFixedMenuResponse
+import com.eatssu.android.data.remote.dto.response.MenuInformationList
+import com.eatssu.android.data.remote.service.MenuService
+import com.eatssu.android.test.AppBehaviorSpec
+import com.eatssu.common.enums.Restaurant
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MenuRepositoryImplBehaviorSpec : AppBehaviorSpec({
+
+    given("MenuRepositoryImpl") {
+        val menuService = mockk<MenuService>()
+        val repository = MenuRepositoryImpl(menuService)
+
+        `when`("고정 메뉴 API가 성공하면") {
+            val response = GetFixedMenuResponse(
+                categoryMenuListCollection = arrayListOf(
+                    CategoryMenuListCollection(
+                        category = "A",
+                        menus = arrayListOf(
+                            MenuInformationList(
+                                menuId = 1L,
+                                name = "돈까스",
+                                price = 5000,
+                                rating = 4.5,
+                            )
+                        ),
+                    )
+                )
+            )
+            coEvery { menuService.getFixMenu(Restaurant.FOOD_COURT.toString()) } returns ApiResult.Success(response)
+
+            then("도메인 Menu 리스트로 매핑한다") {
+                runTest {
+                    val result = repository.getFixedMenuList(Restaurant.FOOD_COURT)
+                    result.size shouldBe 1
+                    result.first().name shouldBe "돈까스"
+                }
+            }
+        }
+
+        `when`("고정 메뉴 API가 실패하면") {
+            coEvery {
+                menuService.getFixMenu(Restaurant.FOOD_COURT.toString())
+            } returns ApiResult.Failure(500, "err")
+
+            then("빈 리스트를 반환한다") {
+                runTest {
+                    repository.getFixedMenuList(Restaurant.FOOD_COURT) shouldBe emptyList()
+                }
+            }
+        }
+    }
+})
---
+++ app/src/test/java/com/eatssu/android/data/remote/repository/PartnershipRepositoryImplBehaviorSpec.kt
@@ -0,0 +1,116 @@
+package com.eatssu.android.data.remote.repository
+
+import com.eatssu.android.data.model.ApiResult
+import com.eatssu.android.data.remote.dto.response.PartnershipResponse
+import com.eatssu.android.data.remote.dto.response.PartnershipRestaurantResponse
+import com.eatssu.android.data.remote.service.PartnershipService
+import com.eatssu.android.data.remote.service.UserService
+import com.eatssu.android.test.AppBehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class PartnershipRepositoryImplBehaviorSpec : AppBehaviorSpec({
+
+    given("PartnershipRepositoryImpl") {
+        val partnershipService = mockk<PartnershipService>()
+        val userService = mockk<UserService>()
+        val repository = PartnershipRepositoryImpl(partnershipService, userService)
+
+        `when`("전체 제휴 조회 API가 성공하면") {
+            val response = listOf(
+                PartnershipResponse(
+                    storeName = "Cafe A",
+                    longitude = 127.0,
+                    latitude = 37.0,
+                    restaurantType = "CAFE",
+                    partnershipInfos = listOf(
+                        PartnershipResponse.PartnershipInfo(
+                            id = 1,
+                            partnershipType = "DISCOUNT",
+                            collegeName = "IT",
+                            departmentName = "CS",
+                            likeCount = 1,
+                            isLiked = true,
+                            description = "10% 할인",
+                            startDate = "2025-01-01",
+                            endDate = "2025-12-31",
+                        )
+                    ),
+                )
+            )
+            coEvery { partnershipService.getAllPartnerships() } returns ApiResult.Success(response)
+
+            then("도메인 Partnership 리스트를 반환한다") {
+                runTest {
+                    val result = repository.getAllPartnerships()
+                    result.size shouldBe 1
+                    result.first().storeName shouldBe "Cafe A"
+                }
+            }
+        }
+
+        `when`("개별 제휴 조회 API가 성공하면") {
+            val response = PartnershipRestaurantResponse(
+                id = 1,
+                partnershipType = "DISCOUNT",
+                storeName = "Cafe A",
+                description = "10% 할인",
+                startDate = "2025-01-01",
+                endDate = "2025-12-31",
+                restaurantType = "CAFE",
+                longitude = 127.0,
+                latitude = 37.0,
+                collegeName = "IT",
+                departmentName = "CS",
+                partnershipLikeCount = 1,
+                likedByUser = true,
+            )
+            coEvery { partnershipService.getPartnershipById(1) } returns ApiResult.Success(response)
+
+            then("도메인 PartnershipRestaurant를 반환한다") {
+                runTest {
+                    val result = repository.getPartnershipById(1)
+                    result?.id shouldBe 1
+                    result?.storeName shouldBe "Cafe A"
+                    result?.description shouldBe "10% 할인"
+                    result?.collegeName shouldBe "IT"
+                }
+            }
+        }
+
+        `when`("유저 학과 제휴 조회가 실패하면") {
+            coEvery { userService.getUserDepartmentPartnerships() } returns ApiResult.Failure(500, "err")
+
+            then("빈 리스트를 반환한다") {
+                runTest {
+                    repository.getUserCollegePartnerships() shouldBe emptyList()
+                }
+            }
+        }
+
+        `when`("유저 학과 제휴 조회가 성공하면") {
+            val response = listOf(
+                PartnershipResponse(
+                    storeName = "Cafe B",
+                    longitude = 127.0,
+                    latitude = 37.0,
+                    restaurantType = "CAFE",
+                    partnershipInfos = emptyList(),
+                )
+            )
+            coEvery { userService.getUserDepartmentPartnerships() } returns ApiResult.Success(response)
+
+            then("도메인 Partnership 리스트를 반환한다") {
+                runTest {
+                    val result = repository.getUserCollegePartnerships()
+                    result.size shouldBe 1
+                    result.first().storeName shouldBe "Cafe B"
+                }
+            }
+        }
+    }
+})
