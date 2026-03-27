0cdc5a8e89ef93a525fd86d6b464d923f1ccbff1
Yu Jin <qldls0307@naver.com>
2026-03-27 14:35:39 +0900

    style: 다이얼로그 그림자 제거 및 스크림 적용

--- core/design-system/src/main/java/com/eatssu/design_system/component/EatSsuDialog.kt
+++ core/design-system/src/main/java/com/eatssu/design_system/component/EatSsuDialog.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,12 +42,6 @@ fun EatSsuDialog(
         onDismissRequest = onDismissRequest,
     ) {
         Surface(
-            modifier = modifier.shadow(
-                elevation = 16.dp,
-                shape = RoundedCornerShape(28.dp),
-                ambientColor = Black.copy(alpha = 0.3f),
-                spotColor = Black.copy(alpha = 0.3f)
-            ),
             shape = RoundedCornerShape(28.dp),
             color = White
         ) {
@@ -132,12 +125,6 @@ fun EatSsuWarningDialog(
         onDismissRequest = onDismissRequest,
     ) {
         Surface(
-            modifier = modifier.shadow(
-                elevation = 16.dp,
-                shape = RoundedCornerShape(28.dp),
-                ambientColor = Black.copy(alpha = 0.3f),
-                spotColor = Black.copy(alpha = 0.3f)
-            ),
             shape = RoundedCornerShape(28.dp),
             color = White
         ) {
