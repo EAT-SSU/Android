f8ec64f3779b84fe9f895649639c16ff75249c5d
PeraSite <pretocki3@gmail.com>
2026-04-09 15:33:42 +0900

    fix: 유닛 테스트 타입 오류 수정

--- app/src/test/java/com/eatssu/android/presentation/intro/IntroViewModelBehaviorSpec.kt
+++ app/src/test/java/com/eatssu/android/presentation/intro/IntroViewModelBehaviorSpec.kt
@@ -3,6 +3,8 @@ package com.eatssu.android.presentation.intro
 import app.cash.turbine.test
 import com.eatssu.android.BuildConfig
 import com.eatssu.android.R
+import com.eatssu.android.data.local.AppThemePreferences
+import com.eatssu.android.domain.model.AppTheme
 import com.eatssu.android.domain.repository.FirebaseRemoteConfigRepository
 import com.eatssu.android.domain.usecase.auth.GetAccessTokenUseCase
 import com.eatssu.android.domain.usecase.health.HealthCheckUseCase
@@ -16,6 +18,7 @@ import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runTest
@@ -28,9 +31,14 @@ class IntroViewModelBehaviorSpec : AppBehaviorSpec({
         val healthCheckUseCase = mockk<HealthCheckUseCase>()
         val getAccessTokenUseCase = mockk<GetAccessTokenUseCase>()
         val firebaseRemoteConfigRepository = mockk<FirebaseRemoteConfigRepository>()
+        val appThemePreferences = mockk<AppThemePreferences>()
+
+        every { appThemePreferences.getAppTheme() } returns AppTheme.DEFAULT
+        every { appThemePreferences.setAppTheme(any()) } returns Unit
 
         `when`("강제 업데이트가 필요하고 토큰이 유효하면") {
             val minimumVersion = (BuildConfig.VERSION_CODE + 1).toLong()
+            coEvery { firebaseRemoteConfigRepository.getAppTheme() } returns AppTheme.DEFAULT
             coEvery { firebaseRemoteConfigRepository.getMinimumVersionCode() } returns minimumVersion
             coEvery { healthCheckUseCase() } returns true
             every { getAccessTokenUseCase() } returns "valid-token"
@@ -39,6 +47,7 @@ class IntroViewModelBehaviorSpec : AppBehaviorSpec({
                 healthCheckUseCase = healthCheckUseCase,
                 getAccessTokenUseCase = getAccessTokenUseCase,
                 firebaseRemoteConfigRepository = firebaseRemoteConfigRepository,
+                appThemePreferences = appThemePreferences,
             )
 
             then("강제 업데이트 결과와 유효 토큰 상태가 반영된다") {
@@ -52,6 +61,7 @@ class IntroViewModelBehaviorSpec : AppBehaviorSpec({
         }
 
         `when`("헬스체크가 실패하면") {
+            coEvery { firebaseRemoteConfigRepository.getAppTheme() } returns AppTheme.DEFAULT
             coEvery { firebaseRemoteConfigRepository.getMinimumVersionCode() } returns BuildConfig.VERSION_CODE.toLong()
             coEvery { healthCheckUseCase() } returns false
             every { getAccessTokenUseCase() } returns "unused"
@@ -60,6 +70,7 @@ class IntroViewModelBehaviorSpec : AppBehaviorSpec({
                 healthCheckUseCase = healthCheckUseCase,
                 getAccessTokenUseCase = getAccessTokenUseCase,
                 firebaseRemoteConfigRepository = firebaseRemoteConfigRepository,
+                appThemePreferences = appThemePreferences,
             )
 
             then("유효 토큰 성공 상태로 전이되지 않는다") {
@@ -72,6 +83,7 @@ class IntroViewModelBehaviorSpec : AppBehaviorSpec({
         }
 
         `when`("헬스체크 성공이지만 access token이 비어있으면") {
+            coEvery { firebaseRemoteConfigRepository.getAppTheme() } returns AppTheme.DEFAULT
             coEvery { firebaseRemoteConfigRepository.getMinimumVersionCode() } returns BuildConfig.VERSION_CODE.toLong()
             coEvery { healthCheckUseCase() } coAnswers {
                 delay(50)
@@ -83,6 +95,7 @@ class IntroViewModelBehaviorSpec : AppBehaviorSpec({
                 healthCheckUseCase = healthCheckUseCase,
                 getAccessTokenUseCase = getAccessTokenUseCase,
                 firebaseRemoteConfigRepository = firebaseRemoteConfigRepository,
+                appThemePreferences = appThemePreferences,
             )
 
             then("토큰 오류 토스트를 보내고 Error 상태가 된다") {
@@ -99,6 +112,7 @@ class IntroViewModelBehaviorSpec : AppBehaviorSpec({
         }
 
         `when`("버전 체크 중 예외가 발생해도") {
+            coEvery { firebaseRemoteConfigRepository.getAppTheme() } returns AppTheme.DEFAULT
             coEvery { firebaseRemoteConfigRepository.getMinimumVersionCode() } throws IllegalStateException("boom")
             coEvery { healthCheckUseCase() } returns true
             every { getAccessTokenUseCase() } returns "valid-token"
@@ -107,6 +121,7 @@ class IntroViewModelBehaviorSpec : AppBehaviorSpec({
                 healthCheckUseCase = healthCheckUseCase,
                 getAccessTokenUseCase = getAccessTokenUseCase,
                 firebaseRemoteConfigRepository = firebaseRemoteConfigRepository,
+                appThemePreferences = appThemePreferences,
             )
 
             then("자동 로그인은 계속 진행되어 성공 상태가 된다") {
@@ -115,6 +130,61 @@ class IntroViewModelBehaviorSpec : AppBehaviorSpec({
                         viewModel.versionCheckResult.value shouldBe null
                         viewModel.uiState.value shouldBe UiState.Success(IntroState.ValidToken)
                     }
+                }
+            }
+        }
+
+        `when`("로컬에 저장된 테마가 spring이고 원격 테마가 나중에 default로 오면") {
+            every { appThemePreferences.getAppTheme() } returns AppTheme.SPRING
+            coEvery { firebaseRemoteConfigRepository.getAppTheme() } coAnswers {
+                delay(100)
+                AppTheme.DEFAULT
+            }
+            coEvery { firebaseRemoteConfigRepository.getMinimumVersionCode() } returns BuildConfig.VERSION_CODE.toLong()
+            coEvery { healthCheckUseCase() } returns true
+            every { getAccessTokenUseCase() } returns "valid-token"
+
+            val viewModel = IntroViewModel(
+                healthCheckUseCase = healthCheckUseCase,
+                getAccessTokenUseCase = getAccessTokenUseCase,
+                firebaseRemoteConfigRepository = firebaseRemoteConfigRepository,
+                appThemePreferences = appThemePreferences,
+            )
+
+            then("초기에는 캐시된 spring을 노출하고 이후 default를 저장한다") {
+                runTest {
+                    viewModel.appTheme.value shouldBe AppTheme.SPRING
+
+                    eventually(2.seconds) {
+                        viewModel.appTheme.value shouldBe AppTheme.DEFAULT
+                    }
+
+                    verify { appThemePreferences.setAppTheme(AppTheme.DEFAULT) }
+                }
+            }
+        }
+
+        `when`("원격 테마 로드에 실패하면") {
+            every { appThemePreferences.getAppTheme() } returns AppTheme.SPRING
+            coEvery { firebaseRemoteConfigRepository.getAppTheme() } throws IllegalStateException("theme-fail")
+            coEvery { firebaseRemoteConfigRepository.getMinimumVersionCode() } returns BuildConfig.VERSION_CODE.toLong()
+            coEvery { healthCheckUseCase() } returns true
+            every { getAccessTokenUseCase() } returns "valid-token"
+
+            val viewModel = IntroViewModel(
+                healthCheckUseCase = healthCheckUseCase,
+                getAccessTokenUseCase = getAccessTokenUseCase,
+                firebaseRemoteConfigRepository = firebaseRemoteConfigRepository,
+                appThemePreferences = appThemePreferences,
+            )
+
+            then("캐시된 테마를 유지하고 저장값은 덮어쓰지 않는다") {
+                runTest {
+                    eventually(2.seconds) {
+                        viewModel.appTheme.value shouldBe AppTheme.SPRING
+                    }
+
+                    verify(exactly = 0) { appThemePreferences.setAppTheme(any()) }
                 }
             }
         }
