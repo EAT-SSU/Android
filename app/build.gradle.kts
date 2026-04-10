9eb1e759c2323bed1c4888e30680f1f6810145e7
PeraSite <pretocki3@gmail.com>
2026-04-10 10:15:23 +0900

    release: 3.2.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.eatssu.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 62
-        versionName = "3.2.9"
+        versionCode = 63
+        versionName = "3.2.10"
 
       testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
